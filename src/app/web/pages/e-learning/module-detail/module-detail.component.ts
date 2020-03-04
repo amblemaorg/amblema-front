4ebc659989427b8d8760c919e57db45a6e811b6c
yaniorchemar <yaniorzambrano@gmail.com>
2020-03-04 08:02:30 -0400

    consumiendo endpoints de modulo de aprendizaje

--- src/app/web/pages/e-learning/module-detail/module-detail.component.ts
+++ src/app/web/pages/e-learning/module-detail/module-detail.component.ts
@@ -4,6 +4,7 @@ import { OwlCarousel } from 'ngx-owl-carousel';
 import { faArrowLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { ModulesService } from '../../../../services/e-learning/modules.service';
 import { GlobalService } from '../../../../services/global.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-module-detail',
@@ -17,6 +18,7 @@ export class ModuleDetailComponent implements OnInit {
   //? quizz area ------------------------------------------------
   moduleCoins = 4;
   completedModule = false;
+  optionsLetters = ['optionA','optionB','optionC','optionD']
   //? -----------------------------------------------------------
 
   faArrowLeft = faArrowLeft;
@@ -42,12 +44,13 @@ export class ModuleDetailComponent implements OnInit {
   stackOps:any;
 
   // PREGUNTAS DEL QUIZZ
-  questions = [
-    { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 0, },
-    { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 1, },
-    { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 2, },
-    { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 3, },
-  ];
+  questions:any;
+  // questions = [
+  //   { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 0, },
+  //   { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 1, },
+  //   { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 2, },
+  //   { desc: 'Lorem ipsum', options: ['Respuesta','Respuesta','Respuesta','Respuesta',], answer: 3, },
+  // ];
 
   selectedQuestions = [];
   incorrectOnes = [];
@@ -57,13 +60,20 @@ export class ModuleDetailComponent implements OnInit {
   isPortrait = true;
   isLandscapeCurrent = false;
 
-  constructor(private moduleService: ModulesService, private globals: GlobalService, @Inject(DOCUMENT) private document: Document) { 
+  constructor(private moduleService: ModulesService, private globals: GlobalService, @Inject(DOCUMENT) private document: Document,
+              private route: ActivatedRoute) { 
     this.isBrowser = globals.isBrowser;    
   }
 
   ngOnInit() {
-    this.selectedQuestions = this.questions.map(i => {return -1});
-    this.incorrectOnes = this.selectedQuestions.slice();
+    let modId = this.route.snapshot.params.id;
+    this.moduleService.getMod(modId).subscribe(res=>{
+      console.log(res);
+      this.questions = res.quizzes;
+      this.selectedQuestions = this.questions.map(i => {return 'option0'});
+      this.incorrectOnes = this.selectedQuestions.slice();
+    });
+    
     this.document.getElementById('completed-message').setAttribute('style','display:block; opacity:0');
     setTimeout(()=>{
       this.document.getElementById('completed-message').setAttribute('style','display:none; opacity:1');
@@ -84,22 +94,35 @@ export class ModuleDetailComponent implements OnInit {
     return i==0? 'A': i==1? 'B': i==2? 'C':'D'
   }
 
+  getOption(j,q) {
+    switch (j) {
+      case 0:
+        return q.optionA;
+      case 1:
+        return q.optionB;
+      case 2:
+        return q.optionC;
+      default:
+        return q.optionD;
+    }
+  }
+
   //? Function called when validate button is pressed
   showModal(el,wm) {
     let success = true; // there are not unselected questions
     let wrong = false; // there are not wrong answers
-    this.incorrectOnes = this.questions.map(i => {return -1}); // re-initializing incorrect answers array
+    this.incorrectOnes = this.questions.map(i => {return 'option0'}); // re-initializing incorrect answers array
     let wrongOnes = this.incorrectOnes.slice(); // temporary incorrect array
 
     for (let i = 0; i < this.selectedQuestions.length; i++) {      
-      if (this.selectedQuestions[i]==-1) {
+      if (this.selectedQuestions[i]=='option0') {
         success = false; // there is at least an unanswered question
         this.showFillAll = true;
         wm.click(); //opening warning modal
         break;
       }
       else {
-        if ( this.selectedQuestions[i]!=this.questions[i].answer ) {
+        if ( this.selectedQuestions[i]!=this.questions[i].correctOption ) {
           wrong = true; // there is at least a wrong answer
           wrongOnes[i] = this.selectedQuestions[i]; // setting wrong answer in the temporary array 
         }
