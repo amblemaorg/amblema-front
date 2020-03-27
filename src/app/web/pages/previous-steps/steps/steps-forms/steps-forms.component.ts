11c47bb0085da3e94ff63f2bfd82d4bad74bec92
yaniorchemar <yaniorzambrano@gmail.com>
2020-03-27 18:31:51 -0400

    stilos a ormularios de pasos previos

--- src/app/web/pages/previous-steps/steps/steps-forms/steps-forms.component.ts
+++ src/app/web/pages/previous-steps/steps/steps-forms/steps-forms.component.ts
@@ -16,27 +16,45 @@ export const VIDEO_PTTRN = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/
 export class StepsFormsComponent implements OnInit {
   @Input() who:string;
 
+  doc_type = [
+    {id:'1',name:'J'},
+    // {name:'V'},
+  ];  
+
   statesExample = [
     {id:'1',name:'Yaracuy'},
     {id:'2',name:'Lara'},
     {id:'3',name:'Zulia'},
   ];
+  municipalitiesExample = [
+    {id:'1',name:'Iribaren'},
+    {id:'2',name:'Cocorote'},
+    {id:'3',name:'Sucre'},
+  ];
+  companyTypes = [
+    {id:'1',name:'Fabrica'},
+    {id:'2',name:'Tienda'},
+    {id:'3',name:'Negocio personal'},
+    {id:'4',name:'Otro'},
+  ];
 
   sponsorForm = this.fb.group({
+    selectedDoc: ['J'],
     name: ['', [Validators.required, Validators.pattern(LETTERS_PTTRN)]],
     email: ['', [Validators.required, Validators.email, Validators.pattern(EMAIL_PTTRN)]],
     rif: ['', [Validators.required, Validators.pattern(NUMBER_PTTRN)]],
     addressState: ['', [Validators.required]],
-    companyType: [''],
-    companyOtherType: [''],
-    phone: [''],
-    address: [''],    
-    addressMunicipality: [''],
-    addressCity: [''],
-    addressStreet: [''],
-    contactFirstName: [''],
-    contactLastName: [''],
-    contactPhone: [''],
+    addressMunicipality: ['', [Validators.required]],
+    addressStreet: ['', [Validators.required]],
+    addressCity: ['', [Validators.required]],  
+    phone: ['', [Validators.required, Validators.pattern(NUMBER_PTTRN)]], 
+    companyType: ['', [Validators.required]], //'1'=fabrica,'2'='tienda',3='negocio personal', 4='otro'
+    contactFirstName: ['', [Validators.required, Validators.pattern(LETTERS_PTTRN)]],
+    contactLastName: ['', [Validators.required, Validators.pattern(LETTERS_PTTRN)]],
+    contactPhone: ['', [Validators.required, Validators.pattern(NUMBER_PTTRN)]],
+    //
+    companyOtherType: [''],    
+    address: [''],                 
     hasSchool: [false],
     schoolName: [''],
     schoolCode: [''],
