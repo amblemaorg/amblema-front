b5bfe8e467bd448a5b520eceb860020d900e6735
yaniorchemar <yaniorzambrano@gmail.com>
2020-04-24 17:56:01 -0400

    seeando archivos de testin g para loscomponentes de textos y botones, formulario y acordeon

--- src/app/peca/peca-page/blocks/texts-buttons-set-block/texts-buttons-set-block.component.spec.ts
+++ src/app/peca/peca-page/blocks/texts-buttons-set-block/texts-buttons-set-block.component.spec.ts
@@ -1,64 +1,69 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBlockComponent } from './form-block.component';
+import { TextsButtonsSetBlockComponent } from './texts-buttons-set-block.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { PageBlockFactory } from '../page-block-factory';
 import { ComponentFactoryResolver, ComponentFactory } from '@angular/core';
 import { PageBlockComponent } from '../page-block.component';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
-import { requiredAndNormalText } from '../../../../web/shared/forms/custom-validators';
-import { MESSAGES } from '../../../../web/shared/forms/validation-messages';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-describe('FormBlockComponent', () => {
-  let component: FormBlockComponent;
-  let fixture: ComponentFixture<FormBlockComponent>;
+describe('TextsButtonsSetBlockComponent', () => {
+  let component: TextsButtonsSetBlockComponent;
+  let fixture: ComponentFixture<TextsButtonsSetBlockComponent>;
 
   let factoryResolver: ComponentFactoryResolver;
   let pageBlockFactory: ComponentFactory<PageBlockComponent>;
   //factoryResolver.resolveComponentFactory.and.returnValue(pageBlockFactory)
 
   let factory: PageBlockFactory = new PageBlockFactory(factoryResolver);
-  let formSettings = {
-    component: 'form',
+  let textsAndButtonsSettings = {
+    component: 'buttons',
     settings: {
-        formsContent: {
-            name: { 
-                label: "Nombre del campo", 
-                placeholder: "Nombre del campo",
-                type:        'text',
-                validations: requiredAndNormalText,
-                messages:    { pattern: MESSAGES.TEXT_MESSAGE }
-            },    
-        }
+        date: {
+            text: 'texto fecha',
+            date: '12/12/12',
+          },
+          status: 'pendiente',
+          // texts: {
+            title: {
+              aligning: 'center', // 'center' for center aligning, 'left' otherwise
+              text: 'texto alineado',
+            },
+            subtitles: [{
+              title: 'subtitulo', // subtitle
+              text: 'parrafo', // paragraph
+            },],
+          // }[];    
+          action: {
+              type: 1, // 1 send, 2 save
+              name: 'Enviar', // text in the button
+          },
+          // upload: any;
+        //   download: any;
     }
   }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        FormBlockComponent,
+        TextsButtonsSetBlockComponent,
       ],
       imports: [
         RouterTestingModule.withRoutes([]),
-        ReactiveFormsModule,
-        FormsModule,
       ]
     })
 
     TestBed.overrideModule(BrowserDynamicTestingModule, {
       set: {
-        entryComponents: [FormBlockComponent]
+        entryComponents: [TextsButtonsSetBlockComponent]
       }
     });
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FormBlockComponent);
+    fixture = TestBed.createComponent(TextsButtonsSetBlockComponent);
     component = fixture.componentInstance;
     factoryResolver = fixture.debugElement.injector.get(ComponentFactoryResolver);
-    component.setSettings(formSettings.settings);
-    // component.isTest = true;
-    // component.setFormBuilding();
+    component.setSettings(textsAndButtonsSettings.settings);
     fixture.detectChanges();
   });
 
