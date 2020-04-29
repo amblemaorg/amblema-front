ec0fddbcd57e4067b23573185deb7e5374a14ccf
joseaguerrero <jose.guerrero@binaural.com.ve>
2020-04-29 14:40:58 -0400

    IMP: informacion de contacto en el footer

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,27 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 // Modules
-import { CommonModule } from '@angular/common';
-import { WebRoutingModule } from './web-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { AngularSvgIconModule } from 'angular-svg-icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha';
+import { CommonModule } from "@angular/common";
+import { WebRoutingModule } from "./web-routing.module";
+import { SharedModule } from "./shared/shared.module";
+import { AngularSvgIconModule } from "angular-svg-icon";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CarouselModule } from "ngx-owl-carousel-o";
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from "ng-recaptcha";
 // Components
-import { WebComponent } from './web.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { MenuComponent } from './layout/menu/menu.component';
-import { FooterComponent } from './layout/footer/footer.component';
+import { WebComponent } from "./web.component";
+import { HeaderComponent } from "./layout/header/header.component";
+import { MenuComponent } from "./layout/menu/menu.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 // Enviroment
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  declarations: [
-    WebComponent,
-    HeaderComponent,
-    MenuComponent,
-    FooterComponent,
-  ],
+  declarations: [WebComponent, HeaderComponent, MenuComponent, FooterComponent],
   imports: [
     CommonModule,
     WebRoutingModule,
@@ -31,9 +27,10 @@ import { environment } from 'src/environments/environment';
     AngularSvgIconModule,
     CarouselModule,
     RecaptchaV3Module,
+    FontAwesomeModule,
   ],
   providers: [
     { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.recaptchaKey },
   ],
 })
-export class WebModule { }
+export class WebModule {}
