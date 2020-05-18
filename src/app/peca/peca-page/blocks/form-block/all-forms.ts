5f1c3744420db29fbce25f66d9e523d7b06bb629
Alfredo <alfredo@binauraldev.com>
2020-05-18 13:27:19 -0400

    diseno html de taller inicial, diseno html datos escuela, responsive general

--- src/app/peca/peca-page/blocks/form-block/all-forms.ts
+++ src/app/peca/peca-page/blocks/form-block/all-forms.ts
@@ -173,16 +173,274 @@ export const sampleFormData = {
           { id: "3", name: "E" },
         ],
       },
-      prependInput: { label: "Input group description", placeholder: "Input group description", fullwidth: false, ...controlProps.numberAndRequired },      
+      prependInput: { label: "Input group description", placeholder: "documento de identidad", fullwidth: false, ...controlProps.numberAndRequired },      
     },
   },
 }
 
+/*TALLER INICIAL*/
+export const formPreparacionTallerInicial = {
+  letterNumber: { label: "Input letters and numbers text", placeholder: "Registro del lugar de taller", fullwidth: false, ...controlProps.onlyLettersNumbersAndRequired },
+  date: { label: "Input date", placeholder: "Fecha del taller", fullwidth: false, ...controlProps.dateAndRequired },
+}
+export const formRegistroInicial = {
+  title1: { label: "Resumen del taller inicial", type: "title" },
+  normal: { label: "Input normal text", placeholder: "Descripción del taller inicial", fullwidth: true, ...controlProps.normalTextAndRequired },
+  title4: { label: "Imágenes del taller inicial", type: "title" },
+  imageGroup: {
+    type: "image",
+    fields: {
+      imageDescription: { label: "Input image description", placeholder: "Descripción de la imágen", fullwidth: false, ...controlProps.normalText },
+      imageStatus: {
+        label: "Input image status",
+        placeholder: "Estado de la imágen",
+        fullwidth: false,
+        ...controlProps.select,
+        options: [
+          { id: "1", name: "Visible" },
+          { id: "2", name: "No visible" },
+        ],
+      },
+    },
+  },
+}
+/*DATOS DE LA ESCUELA*/
+export const formDatosEscuela = {
+  title1: { label: "Datos básicos de la escuela", type: "title" },
+  nameEscuela: { label: "Input letter text", placeholder: "Nombre de la escuela", readonly:true, fullwidth: false,...controlProps.onlyLettersNumbers},
+  codigoEscuela: { label: "Input letter text", placeholder: "Código del plantel", readonly:true, fullwidth: false,...controlProps.onlyLettersNumbers},
+  phoneEscuela: { label: "Input phone text", placeholder: "Teléfono", fullwidth: false, readonly:true,...controlProps.phone},
+  estadoEscuela: { label: "Input letter text", placeholder: "Estado", readonly:true, fullwidth: false,...controlProps.onlyLettersNumbers},
+  municipioEscuela: { label: "Input letter text", placeholder: "Municipio", readonly:true,fullwidth: false,...controlProps.onlyLettersNumbers},
+  callesEscuela: { label: "Input letter text", placeholder: "Calles / carreras", readonly:true, fullwidth: false,...controlProps.onlyLettersNumbers},
+  ciudadEscuela: { label: "Input letter text", placeholder: "Ciudad", readonly:true,fullwidth: false,...controlProps.onlyLettersNumbers},
+  /*DATOS DIRECTOR*/
+  title2: { label: "Datos del director", type: "title" },
+  nameDirector: { label: "Input normal text", placeholder: "Nombre del director o directora ", fullwidth: false, ...controlProps.normalTextAndRequired },
+  phoneDirector: { label: "Input phone text", placeholder: "Número de teléfono del director", fullwidth: false,...controlProps.phoneAndRequired},
+  emailDirector: { label: "Input email text", placeholder: "Correo electrónico del director", fullwidth: false, ...controlProps.emailAndRequired },
+  /*DATOS SUB-DIRECTOR*/
+  title3: { label: "Datos del subdirector", type: "title" },
+  nameSubDirector: { label: "Input normal text", placeholder: "Nombre del subdirector o subdirectora ", fullwidth: false, ...controlProps.normalTextAndRequired },
+  phoneSubDirector: { label: "Input phone text", placeholder: "Número de teléfono del subdirector", fullwidth: false,...controlProps.phoneAndRequired},
+  emailSubDirector: { label: "Input email text", placeholder: "Correo electrónico del subdirector", fullwidth: false, ...controlProps.emailAndRequired },
+  /*Información adicional*/
+  title4: { label: "Información adicional", type: "title" },
+  number1: { label: "Input number text", placeholder: "Cantidad de docentes", readonly:true, fullwidth2: true, ...controlProps.number },
+  number2: { label: "Input number text", placeholder: "Cantidad de personal administrativo", readonly:true,fullwidth2: true, ...controlProps.number },
+  number3: { label: "Input number text", placeholder: "Cantidad de personal obrero", readonly:true,fullwidth2: true, ...controlProps.number },
+  number4: { label: "Input number text", placeholder: "Cantidad de estudiantes", readonly:true, fullwidth2: true, ...controlProps.number },
+  number5: { label: "Input number text", placeholder: "Cantidad de grados", readonly:true,fullwidth2: true, ...controlProps.number },
+  facebook: { label: "Input normal text", placeholder: "Facebook", fullwidth2: true, ...controlProps.normalTextAndRequired },
+  instagram: { label: "Input normal text", placeholder: "Instagram", fullwidth2: true, ...controlProps.normalTextAndRequired },
+  twitter: { label: "Input normal text", placeholder: "Twitter", fullwidth2: true, ...controlProps.normalTextAndRequired },
+}
+export const formImgEscuela = {
+  title5: { label: "Slider principal de la escuela", type: "title" },
+  imageGroup: {
+    type: "image",
+    fields: {
+      imageDescription: { label: "Input image description", placeholder: "Descripción de la imágen", fullwidth: false, ...controlProps.normalText },
+      imageStatus: {
+        label: "Input image status",
+        placeholder: "Estatus",
+        fullwidth: false,
+        ...controlProps.select,
+        options: [
+          { id: "1", name: "Visible" },
+          { id: "2", name: "No visible" },
+        ],
+      },
+    },
+  },
+}
+/*TABS DOCENTE*/
+export const formTabsDocente = {
+  titleName: { label: "Datos del Docente", type: "title" },
+  nameDocente: { label: "Nombre del Docente", placeholder: "Nombre", fullwidth: false, ...controlProps.onlyLettersAndRequired },
+  lastNameDocente: { label: "Apellido del Docente", placeholder: "Apellido", fullwidth: false, ...controlProps.onlyLettersAndRequired },
+  documentGroup: {
+    type: "prepend",
+    fields: {
+      prependSelect: {
+        label: "Input group select",
+        placeholder: "Input group select",
+        fullwidth: false,
+        ...controlProps.select,
+        options: [
+          { id: "1", name: "V" },
+          { id: "2", name: "J" },
+          { id: "3", name: "E" },
+        ],
+      },
+      prependInput: { label: "Input group description", placeholder: "documento de identidad", fullwidth: false, ...controlProps.numberAndRequired },      
+    },
+  },
+  phone: { label: "Input phone text", placeholder: "Teléfono", fullwidth: false, ...controlProps.phoneAndRequired },
+  email: { label: "Input email text", placeholder: "Correo", fullwidth: false, ...controlProps.emailAndRequired },
+  addressState: {
+    label: "Input select state",
+    placeholder: "Estado",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "165146541654hjvjh", name: "Lara" },
+      { id: "165146wfw254hjvjh", name: "Yaracuy" },
+    ],
+  },
+  addressMunicipality: {
+    label: "Input select municipality",
+    placeholder: "Municipio",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      {
+        id: "dgisgsd64646464",
+        name: "Iribarren",
+        state: { id: "165146541654hjvjh", name: "Lara" },
+      },
+      {
+        id: "tdjsgshdge8791654",
+        name: "Palavecinos",
+        state: { id: "165146541654hjvjh", name: "Lara" },
+      },
+      {
+        id: "jgisgsd64646464",
+        name: "Cocorote",
+        state: { id: "165146wfw254hjvjh", name: "Yaracuy" },
+      },
+      {
+        id: "bdjsgshdge8791654",
+        name: "Bruzual",
+        state: { id: "165146wfw254hjvjh", name: "Yaracuy" },
+      },
+    ],
+  },
+  street: { label: "Input normal text", placeholder: "Calles / Carreras", fullwidth: false, ...controlProps.normalTextAndRequired },
+  city: { label: "Input normal text", placeholder: "Ciudad", fullwidth: false, ...controlProps.normalTextAndRequired },
+  status: {
+    label: "Input select sex",
+    placeholder: "Estatus",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "1", name: "Activo" },
+      { id: "2", name: "Inactivo" },
+    ],
+  },
+}
+/*TABS GRADOS Y SECCIONES*/
+export const formGradosSecciones = {
+  grades: {
+    label: "Input select sex",
+    placeholder: "Grados",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "1", name: "1" },
+      { id: "2", name: "2" },
+      { id: "3", name: "3" },
+      { id: "4", name: "4" },
+      { id: "5", name: "5" },
+      { id: "6", name: "6" },
+    ],
+  },
+  section: { label: "Input normal text", placeholder: "Sección", fullwidth: false, ...controlProps.normalTextAndRequired },
+  docentes: {
+    label: "Input select sex",
+    placeholder: "Docentes",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "1", name: "Alfredo" },
+      { id: "2", name: "Yanior" },
+      { id: "3", name: "Stephanie" },
+    ],
+  },
+}
+/*Tabs Estudiantes*/
+export const formTabsEstudiantes = {
+  grades: {
+    label: "Input select grades",
+    placeholder: "Grados",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "1", name: "1" },
+      { id: "2", name: "2" },
+      { id: "3", name: "3" },
+      { id: "4", name: "4" },
+      { id: "5", name: "5" },
+      { id: "6", name: "6" },
+    ],
+  },
+  secction: {
+    label: "Input select section",
+    placeholder: "Sección",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "1", name: "A" },
+      { id: "2", name: "B" },
+      { id: "3", name: "C" },
+      { id: "4", name: "D" },
+      { id: "5", name: "E" },
+      { id: "6", name: "F" },
+    ],
+  },
+  letterName: { label: "Nombre Estudiante", placeholder: "Nombre", fullwidth: false, ...controlProps.onlyLettersAndRequired },
+  lastNameLetter: { label: "Apellido Estudiante", placeholder: "Apellido", fullwidth: false, ...controlProps.onlyLettersAndRequired },
+  documentGroup: {
+    type: "prepend",
+    fields: {
+      prependSelect: {
+        label: "Input group select",
+        placeholder: "Input group select",
+        fullwidth: false,
+        ...controlProps.select,
+        options: [
+          { id: "1", name: "V" },
+          { id: "2", name: "J" },
+          { id: "3", name: "E" },
+        ],
+      },
+      prependInput: { label: "Input group description", placeholder: "documento de identidad", fullwidth: false, ...controlProps.numberAndRequired },      
+    },
+  },
+  date: { label: "Input date", placeholder: "Fecha de nacimiento", fullwidth: false, ...controlProps.dateAndRequired },
+  sexo: {
+    label: "Input select sex",
+    placeholder: "Género (Femenino ó Msculino)",
+    fullwidth: false,
+    ...controlProps.selectAndRequired,
+    options: [
+      { id: "1", name: "Femenino" },
+      { id: "2", name: "Masculino" },
+    ],
+  },
+}
+/* PERFIL DE USUARIO COORDINADOR*/
 export const formCoordinador = {
   titleName: { label: "Datos del Coordinador", type: "title" },
   letter: { label: "Nombre del Coordinador", placeholder: "Nombre del Coordinador", fullwidth: false, ...controlProps.onlyLettersAndRequired },
   lastNameLetter: { label: "Apellido del Coordinador", placeholder: "Apellido del Coordinador", fullwidth: false, ...controlProps.onlyLettersAndRequired },
-  document: { label: "Documento de Identidad", placeholder: "Documento de Identidad", fullwidth: false, ...controlProps.onlyLettersNumbersAndRequired },
+  documentGroup: {
+    type: "prepend",
+    fields: {
+      prependSelect: {
+        label: "Input group select",
+        placeholder: "Input group select",
+        fullwidth: false,
+        ...controlProps.select,
+        options: [
+          { id: "1", name: "V" },
+          { id: "2", name: "J" },
+          { id: "3", name: "E" },
+        ],
+      },
+      prependInput: { label: "Input group description", placeholder: "documento de identidad", fullwidth: false, ...controlProps.numberAndRequired },      
+    },
+  },
   email: { label: "Input email text", placeholder: "Correo", fullwidth: false, ...controlProps.emailAndRequired },
   phoneMovil: { label: "Input phone text", placeholder: "Teléfono móvil", fullwidth: false, ...controlProps.phoneAndRequired },
   phone: { label: "Input phone text", placeholder: "Teléfono de habitación", fullwidth: false, ...controlProps.phoneAndRequired },
@@ -257,11 +515,27 @@ export const formCoordinador = {
     },
   },
 }
-
+/* PERFIL DE USUARIO PADRINO*/
 export const formPadrino = {
   titleName: { label: "Datos de la Empresa", type: "title" },
   letter: { label: "Nombre de la Empresa", placeholder: "Nombre de la Empresa", fullwidth: false, ...controlProps.onlyLettersAndRequired },
-  document: { label: "RIF", placeholder: "RIF", fullwidth: false, ...controlProps.onlyLettersNumbersAndRequired },
+  documentGroup: {
+    type: "prepend",
+    fields: {
+      prependSelect: {
+        label: "Input group select",
+        placeholder: "Input group select",
+        fullwidth: false,
+        ...controlProps.select,
+        options: [
+          { id: "1", name: "V" },
+          { id: "2", name: "J" },
+          { id: "3", name: "E" },
+        ],
+      },
+      prependInput: { label: "Input group description", placeholder: "documento de identidad", fullwidth: false, ...controlProps.numberAndRequired },      
+    },
+  },
   phone: { label: "Input phone text", placeholder: "Teléfono", fullwidth: false, ...controlProps.phoneAndRequired },
   email: { label: "Input email text", placeholder: "Correo", fullwidth: false, ...controlProps.emailAndRequired },
   tipoEmpresa: {
@@ -349,7 +623,7 @@ export const formPadrino = {
     },
   },
 }
-
+/* PERFIL DE USUARIO ESCUELA*/
 export const formEscuela = {
   titleName: { label: "Datos de la Escuela", type: "title" },
   name: { label: "Nombre de la Escuela", placeholder: "Nombre de la Escuela", fullwidth: false, ...controlProps.onlyLettersAndRequired },
--- src/app/peca/peca-page/pages/school-data-config.ts
+++ src/app/peca/peca-page/pages/school-data-config.ts
@@ -1,20 +1,278 @@
+import { formDatosEscuela, formImgEscuela, formTabsDocente, formGradosSecciones, formTabsEstudiantes } from '../blocks/form-block/all-forms';
+
+const textsAndButtons = {
+  component: 'textsbuttons',
+  settings: {
+    title: {
+      text: '(Centrado) Solicitud de asesoria a AmbLeMa: Licencia para operar',
+      aligning: 'center',
+    },
+    action: {
+      type: 1,
+      name: 'Enviar Solicitud',
+    },
+  }
+}
+
+const formEscuela = {
+  component: 'form',
+  settings: {
+    formsContent: formDatosEscuela,
+    buttons: ['guardar'],
+  }
+}
+const formImageEscuela = {
+  component: 'form',
+  settings: {
+    formsContent: formImgEscuela,
+  }
+}
+
+const registroEscuela = {
+  component: 'table',
+  settings: {
+    columns: {
+      image: {
+        title: "Imágenes",
+      },
+      description: {
+        title: "Descripción"
+      },
+      state: {
+        title: 'Estado'
+      },
+      status: {
+        title: 'Estatus'
+      }
+    },
+    data: [
+      {
+        image: '5to Grado',
+        description: 'lorem',
+        state: 'Proceso',
+        status: 'Activo'
+      },
+      {
+        image: '6to Grado',
+        description: 'lorem',
+        state: 'Proceso',
+        status: 'Inactivo'
+      },
+      {
+        image: '4to Grado',
+        description: 'loremlorem',
+        state: 'Proceso',
+        status: 'Activo'
+      },
+    ],
+    classes: {
+      hideView: false,
+      hideEdit: false,
+      hideDelete: false,
+    },
+  }
+}
+const formDocente = {
+  component: 'form',
+  settings: {
+    formsContent: formTabsDocente,
+    buttons: ['guardar'],
+  }
+}
+const tablaDocente = {
+  component: 'table',
+  settings: {
+    columns: {
+      name: {
+        title: "Nombre",
+      },
+      lastName: {
+        title: "Apellido"
+      },
+      identity: {
+        title: 'Cedula'
+      },
+      mail: {
+        title: 'Correo'
+      },
+      status: {
+        title: 'Estatus'
+      }
+    },
+    data: [
+      {
+        name: 'ALfredo',
+        lastName: 'Valbuena',
+        identity: '20017764',
+        mail: 'almavalo11@gmail.com',
+        status: 'Activo'
+      },
+      {
+        name: 'Manuel',
+        lastName: 'Perez',
+        identity: '123456789',
+        mail: 'almavalo@gmail.com',
+        status: 'Activo'
+      },
+      {
+        name: 'Luis',
+        lastName: 'Valbuena',
+        identity: '20017764',
+        mail: 'almavalo@hotmail.com',
+        status: 'Inactivo'
+      },
+    ],
+    classes: {
+      hideView: false,
+      hideEdit: false,
+      hideDelete: false,
+    },
+  }
+}
+const formGradosYSecciones = {
+  component: 'form',
+  settings: {
+    formsContent: formGradosSecciones,
+    buttons: ['guardar'],
+  }
+}
+const tablaGradosSecciones = {
+  component: 'table',
+  settings: {
+    columns: {
+      grades: {
+        title: "Grados",
+      },
+      secctions: {
+        title: "Secciones"
+      },
+      name: {
+        title: 'Docente'
+      },
+    },
+    data: [
+      {
+        grades: '5',
+        secctions: 'B',
+        name: 'Isa',
+      },
+      {
+        grades: '6',
+        secctions: 'A',
+        name: 'Alfredo',
+      },
+      {
+        grades: '3',
+        secctions: 'C',
+        name: 'Manuel',
+      },
+    ],
+    classes: {
+      hideView: false,
+      hideEdit: false,
+      hideDelete: false,
+    },
+  }
+}
+const formEstudiantes = {
+  component: 'form',
+  settings: {
+    formsContent: formTabsEstudiantes,
+    buttons: ['guardar'],
+  }
+}
+const tablaEstudiante = {
+  component: 'table',
+  settings: {
+    columns: {
+      name: {
+        title: "Nombre",
+      },
+      lastName: {
+        title: "Apellido"
+      },
+      doc: {
+        title: 'Cédula'
+      },
+      sex: {
+        title: 'Género'
+      },
+      age: {
+        title: 'Edad'
+      },
+    },
+    data: [
+      {
+        name: 'Isa',
+        lastName: 'Godoy',
+        doc: '23577413',
+        sex: 'Femenino',
+        age: '15',
+      },
+      {
+        name: 'ALfredo',
+        lastName: 'Valbuena',
+        doc: '123456',
+        sex: 'Masculino',
+        age: '12',
+      },
+    ],
+    classes: {
+      hideView: false,
+      hideEdit: false,
+      hideDelete: false,
+    },
+  }
+}
+
 export const SCHOOL_DATA_CONFIG = {
   header: {
     title: "Datos de la escuela"
   },
   blocks: [
     {
-      component: 'tabs',
+      component: 'accordion',
       settings: {
         items: [
           {
-            title: "Grados y sección",
+            title: "Datos de la Escuela",
+            icon: "taller-inicial",
+            childBlocks: [
+              { ...formEscuela },
+              { ...formImageEscuela },
+              { ...registroEscuela },
+              { ...textsAndButtons }
+            ]
           },
           {
-            title: "Estudiantes"
-          }
+            title: "Docentes",
+            icon: "folder-open",
+            childBlocks: [
+              { ...formDocente },
+              { ...tablaDocente }
+            ]
+          },
+          {
+            title: "Grados y secciones",
+            icon: "diagnostico",
+            childBlocks: [
+              { ...formGradosYSecciones },
+              { ...tablaGradosSecciones }
+            ]
+          },
+          {
+            title: "Estudiantes",
+            icon: "planificacion",
+            childBlocks: [
+              { ...formEstudiantes },
+              { ...tablaEstudiante }
+            ]
+          },
         ]
       }
-    }
-  ]
+    },
+  ],
 }
+
+
+
