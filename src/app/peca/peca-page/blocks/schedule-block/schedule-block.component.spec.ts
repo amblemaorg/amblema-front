b7dcaf0f2220514c584870cbe931e33e44cb03b5
Alfredo <alfredo@binauraldev.com>
2020-06-09 17:21:48 -0400

    pruebas componente schedule

--- src/app/peca/peca-page/blocks/schedule-block/schedule-block.component.spec.ts
+++ src/app/peca/peca-page/blocks/schedule-block/schedule-block.component.spec.ts
@@ -1,21 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFactoryResolver, ComponentFactory } from '@angular/core';
+import { PageBlockComponent } from '../page-block.component';
 import { ScheduleBlockComponent } from './schedule-block.component';
+import { PageBlockFactory } from '../page-block-factory';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 
 describe('ScheduleBlockComponent', () => {
   let component: ScheduleBlockComponent;
   let fixture: ComponentFixture<ScheduleBlockComponent>;
 
-  beforeEach(async(() => {
+  let factoryResolver: ComponentFactoryResolver;
+  let pageBlockFactory: ComponentFactory<PageBlockComponent>;
+  //factoryResolver.resolveComponentFactory.and.returnValue(pageBlockFactory)
+
+  let factory: PageBlockFactory = new PageBlockFactory(factoryResolver);
+  let scheduleSettings = {
+    component: 'agendas',
+    settings: {
+      items: [
+        {}
+      ],
+    }
+  }
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ ScheduleBlockComponent ]
+      declarations: [ ScheduleBlockComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        ScheduleModule,
+      ]
     })
-    .compileComponents();
-  }));
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [ScheduleBlockComponent]
+      }
+    });
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ScheduleBlockComponent);
     component = fixture.componentInstance;
+    factoryResolver = fixture.debugElement.injector.get(ComponentFactoryResolver);
+    component.setSettings(scheduleSettings);
     fixture.detectChanges();
   });
 
--- src/app/peca/peca-page/blocks/schedule-block/schedule-block.component.ts
+++ src/app/peca/peca-page/blocks/schedule-block/schedule-block.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, AfterViewInit, ViewContainerRef, ViewChild, QueryList, ViewChildren } from '@angular/core';
-import { PageBlockComponent, StructuralBlockComponent, StructuralItem } from '../page-block.component';
+import { PageBlockComponent, PresentationalBlockComponent } from '../page-block.component';
 import { PageBlockFactory } from '../page-block-factory';
 import { DayService, WeekService, WorkWeekService, MonthService, AgendaService, View, EventSettingsModel } from '@syncfusion/ej2-angular-schedule';
-import { Internationalization,L10n, loadCldr } from '@syncfusion/ej2-base';
+import { Internationalization, L10n, loadCldr } from '@syncfusion/ej2-base';
 import { DataManager, ODataV4Adaptor, Query } from '@syncfusion/ej2-data';
 import * as  numberingSystems from 'cldr-data/supplemental/numberingSystems.json';
 import * as  gregorian from 'cldr-data/main/es/ca-gregorian.json';
@@ -12,15 +12,15 @@ import * as  timeZoneNames from 'cldr-data/main/es/timeZoneNames.json';
 loadCldr(numberingSystems['default'], gregorian['default'], numbers['default'], timeZoneNames['default']);
 L10n.load({
   "es": {
-      "schedule": {
-          "month": "Mes",
-          "agenda": "Agenda",
-          "today": "Hoy",
-          "allDay": "Todo el dia",
-          "close": "Cerrar",
-          "delete": "Eliminar",
-          "edit": "Editar",
-      },
+    "schedule": {
+      "month": "Mes",
+      "agenda": "Agenda",
+      "today": "Hoy",
+      "allDay": "Todo el dia",
+      "close": "Cerrar",
+      "delete": "Eliminar",
+      "edit": "Editar",
+    },
   }
 });
 
@@ -30,17 +30,17 @@ L10n.load({
   templateUrl: './schedule-block.component.html',
   styleUrls: ['./schedule-block.component.scss']
 })
-export class ScheduleBlockComponent implements StructuralBlockComponent, OnInit, AfterViewInit {
+export class ScheduleBlockComponent implements PresentationalBlockComponent, OnInit {
   @ViewChildren('agendaContainer', { read: ViewContainerRef }) agendaContainer: QueryList<ViewContainerRef>;
   factory: PageBlockFactory;
 
-  type: 'structural';
+  type: 'presentational';
   component: string;
   settings: {
-    items: StructuralItem[];
+    items: any[];
   }
   constructor() {
-    this.type = 'structural';
+    this.type = 'presentational';
     this.component = 'agendas';
   }
 
@@ -55,26 +55,26 @@ export class ScheduleBlockComponent implements StructuralBlockComponent, OnInit,
   public eventSettings: EventSettingsModel = { dataSource: this.dataManager, query: this.dataQuery };
 /*--------------------------------------------------------*/
 
-/*COnsumo Local*/
-public data: object[] = [{
-  Id: 1,
-  Subject: 'Meeting',
-  StartTime: new Date(2020, 5, 15),
-  EndTime: new Date(2020, 5, 17),
-  Description: 'Meeting time changed based on team activities.',
-  IsAllDay: true,
-  CategoryColor: "Blue",
-},
-{
-  Id: 2,
-  Subject: 'Scrum Meeting',
-  StartTime: new Date(2020, 5, 30, 9, 0),
-  EndTime: new Date(2020, 5, 30, 10, 30),
-  Description: 'Meeting time changed based on team activities.',
-}];
-public eventSettings: EventSettingsModel = {
-  dataSource: this.data
-}/*
+  /*COnsumo Local*/
+  public data: object[] = [{
+    Id: 1,
+    Subject: 'Meeting',
+    StartTime: new Date(2020, 5, 15),
+    EndTime: new Date(2020, 5, 17),
+    Description: 'Meeting time changed based on team activities.',
+    IsAllDay: true,
+    CategoryColor: "Blue",
+  },
+  {
+    Id: 2,
+    Subject: 'Scrum Meeting',
+    StartTime: new Date(2020, 5, 30, 9, 0),
+    EndTime: new Date(2020, 5, 30, 10, 30),
+    Description: 'Meeting time changed based on team activities.',
+  }];
+  public eventSettings: EventSettingsModel = {
+    dataSource: this.data
+  }/*
 -------------------------------------------------------*/
 
   public allowVirtualScroll: boolean = true;
@@ -84,31 +84,14 @@ public eventSettings: EventSettingsModel = {
     return this.instance.formatDate(value, { skeleton: 'hm' });
   };
   public isReadOnly: boolean = true;
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.instantiateChildBlocks();
-    })
-  }
+
 
   ngOnInit() {
   }
 
-  public setSettings(settings: any) {
-    this.settings = settings.settings;
-    this.factory = settings.factory ? settings.factory : {};
+  setSettings(settings: any) {
+    this.settings = { ...settings };
   }
 
-  public instantiateChildBlocks() {
-    this.settings.items.map((item, i) => {
-      const container = this.agendaContainer.toArray()[i];
-      item.childBlocks.map(block => {
-        let settings = block.settings;
-        if (block.component == "modal") settings = { settings: block.settings, factory: this.factory };
-        const pageBlockComponentFactory = this.factory.createPageBlockFactory(block.component);
-        const pageBlockComponent = container.createComponent(pageBlockComponentFactory);
-        pageBlockComponent.instance.setSettings(settings);
-      })
-    })
-  }
 
 }
