e213ba86353f972e6463433e603f6381c9c19e29
joseaguerrero <jose.guerrero@binaural.com.ve>
2020-06-12 16:49:19 -0400

    IMP: manejo de estado del PECA con ngxs

---
+++ src/app/store/states/peca/peca.model.ts
@@ -0,0 +1,29 @@
+export interface PecaObjectModel {
+  name: string;
+  id: string;
+}
+
+export interface PecaProjectModel {
+  code: string;
+  coordinator: PecaObjectModel;
+  id: string;
+  pecas: any[];
+  phase: string;
+  school: PecaObjectModel;
+  sponsor: PecaObjectModel;
+}
+
+export interface PecaUserModel {
+  activeSchoolYear: PecaObjectModel;
+  email: string;
+  id: string;
+  name: string;
+  permissions: string[];
+  projects: PecaProjectModel[];
+  userType: string;
+}
+
+export interface PecaStateModel {
+  user?: PecaUserModel;
+  selectedProject: PecaProjectModel | null;
+}
