077d5ab2903698b5fca96f064a513f0e4c20ddcb
joseaguerrero <jose.guerrero@binaural.com.ve>
2020-06-12 16:28:18 -0400

    IMP: proteccion de rutas de seleccion de escuela, y pasos previos al peca

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,39 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AuthGuard } from "./guards/auth.guard";
+import { AllowAuthenticatedGuard } from "./guards/allow-authenticated.guard";
 
 const routes: Routes = [
   {
     path: "",
-    loadChildren: () => import("./web/web.module").then(m => m.WebModule)
+    loadChildren: () => import("./web/web.module").then((m) => m.WebModule),
   },
   {
     path: "previous-steps",
+    canActivateChild: [AllowAuthenticatedGuard],
     loadChildren: () =>
-      import("./web/pages/previous-steps/previous-steps.module").then(
-        m => m.PreviousStepsModule
-      )
+      import("./web/pages/previous-steps/previous-steps.module").then((m) => m.PreviousStepsModule),
   },
   {
     path: "peca",
-    canActivate: [AuthGuard],
-    loadChildren: () => import("./peca/peca.module").then(m => m.PecaModule)
+    canActivateChild: [AllowAuthenticatedGuard],
+    loadChildren: () => import("./peca/peca.module").then((m) => m.PecaModule),
   },
   {
     path: "auth",
-    loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
   },
   {
     path: "seleccion-escuela",
+    canActivate: [AllowAuthenticatedGuard],
     loadChildren: () =>
       import("./peca/school-selection/school-selection.module").then(
-        m => m.SchoolSelectionModule
-      )
-  }
+        (m) => m.SchoolSelectionModule
+      ),
+  },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
