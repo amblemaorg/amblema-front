00ba3ec90f8f94860794785b7b55bef8056cd6c9
joseaguerrero <jose.guerrero@binaural.com.ve>
2020-06-17 18:02:15 -0400

    MOD: pruebas de componentes en PECA

--- src/app/peca/school-selection/school-selection.component.spec.ts
+++ src/app/peca/school-selection/school-selection.component.spec.ts
@@ -1,18 +1,27 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { SchoolSelectionComponent } from "./school-selection.component";
-import { RouterTestingModule } from "@angular/router/testing";
-import { NbAuthService, NbTokenService, NbAuthModule } from "@nebular/auth";
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SchoolSelectionComponent } from './school-selection.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NbAuthService, NbTokenService, NbAuthModule } from '@nebular/auth';
+import { Store, NgxsModule } from '@ngxs/store';
+import { PecaState } from 'src/app/store/states/peca/peca.state';
+import { HttpClientModule } from '@angular/common/http';
 
-describe("SchoolSelectionComponent", () => {
+describe('SchoolSelectionComponent', () => {
   let component: SchoolSelectionComponent;
   let fixture: ComponentFixture<SchoolSelectionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SchoolSelectionComponent],
-      imports: [FontAwesomeModule, RouterTestingModule, NbAuthModule.forRoot()],
-      providers: [NbAuthService, NbTokenService]
+      imports: [
+        FontAwesomeModule,
+        RouterTestingModule,
+        HttpClientModule,
+        NbAuthModule.forRoot(),
+        NgxsModule.forRoot([PecaState]),
+      ],
+      providers: [NbAuthService, NbTokenService],
     }).compileComponents();
   }));
 
@@ -22,7 +31,7 @@ describe("SchoolSelectionComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
