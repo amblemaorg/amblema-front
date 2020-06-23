0b8721305fd2e717d3199231118c776c2a14ad37
jesus amaro <jesusamaro1995@gmail.com>
2020-06-23 15:22:02 -0400

    mapeo de la data en los 3 formularios y seteo de la data a los formularios

--- src/app/peca/peca-page/pages/profile-config.ts
+++ src/app/peca/peca-page/pages/profile-config.ts
@@ -1,33 +1,89 @@
-import { formCoordinador } from '../blocks/form-block/all-forms'
-import { formPadrino } from '../blocks/form-block/all-forms'
-import { formEscuela } from '../blocks/form-block/all-forms'
+import { formCoordinador } from "../blocks/form-block/all-forms";
+import { formPadrino } from "../blocks/form-block/all-forms";
+import { formEscuela } from "../blocks/form-block/all-forms";
 
-const formSet = {
-    component: 'form',
-    settings: {
-        formsContent: formCoordinador,
-        buttons: ['guardar,editar,cancelar'],
+const formSetCoordinador = {
+  component: "form",
+  name: "userCordinatorForm",
+  settings: {
+    formsContent: formCoordinador,
+    buttons: ["guardar,editar,cancelar"],
+    data: {}
+  }
+};
+const formSetEscuela = {
+  component: "form",
+  name: "userSchoolForm",
+  settings: {
+    formsContent: formEscuela,
+    buttons: ["guardar,editar,cancelar"],
+    data: {}
+  }
+};
+const formSetPadrino = {
+  component: "form",
+  name: "userSponsorForm",
+  settings: {
+    formsContent: formPadrino,
+    buttons: ["guardar,editar,cancelar"],
+    data: {}
+  }
+};
+
+export const PROFILE_CONFIG_PADRINO = {
+  header: {
+    title: "Perfil de Usuario"
+  },
+  blocks: [
+    {
+      component: "profiles",
+      settings: {
+        items: [
+          {
+            image: "../../assets/images/profile-oscar.jpg",
+            title: "Carlos Camacaro",
+            childBlocks: [{ ...formSetPadrino }]
+          }
+        ]
+      }
     }
-}
-
-export const PROFILE_CONFIG = {
-    header: {
-        title: "Perfil de Usuario"
-    },
-    blocks: [
-        {
-            component: 'profiles',
-            settings: {
-                items: [
-                    {
-                        image: "../../assets/images/profile-oscar.jpg",
-                        title: "Carlos Camacaro",
-                        childBlocks: [
-                            { ...formSet }
-                        ]
-                    }
-                ]
-            }
-        }
-    ]
-}
+  ]
+};
+export const PROFILE_CONFIG_ESCUELA = {
+  header: {
+    title: "Perfil de Usuario"
+  },
+  blocks: [
+    {
+      component: "profiles",
+      settings: {
+        items: [
+          {
+            image: "../../assets/images/profile-oscar.jpg",
+            title: "Carlos Camacaro",
+            childBlocks: [{ ...formSetEscuela }]
+          }
+        ]
+      }
+    }
+  ]
+};
+export const PROFILE_CONFIG_COORDINADOR = {
+  header: {
+    title: "Perfil de Usuario"
+  },
+  blocks: [
+    {
+      component: "profiles",
+      settings: {
+        items: [
+          {
+            image: "../../assets/images/profile-oscar.jpg",
+            title: "Carlos Camacaro",
+            childBlocks: [{ ...formSetCoordinador }]
+          }
+        ]
+      }
+    }
+  ]
+};
--- src/app/peca/peca-page/pages/profile-page.component.ts
+++ src/app/peca/peca-page/pages/profile-page.component.ts
@@ -1,29 +1,153 @@
 import {
-    Component,
-    AfterViewInit,
-    Injector,
-    ViewChild,
-    ViewContainerRef,
-    ComponentFactoryResolver,
-} from '@angular/core';
-import { PecaPageComponent } from '../peca-page.component';
-import { PROFILE_CONFIG as config } from './profile-config';
+  Component,
+  AfterViewInit,
+  Injector,
+  ViewChild,
+  ViewContainerRef,
+  ComponentFactoryResolver
+} from "@angular/core";
+import { PecaPageComponent } from "../peca-page.component";
+import { PROFILE_CONFIG_PADRINO as configPadrino } from "./profile-config";
+import { PROFILE_CONFIG_ESCUELA as configEscuela } from "./profile-config";
+import { PROFILE_CONFIG_COORDINADOR as configCoordinador } from "./profile-config";
+import { PecaState } from "src/app/store/states/peca/peca.state";
+import { Observable, Subscription } from "rxjs";
+import { Select } from "@ngxs/store";
+import { GlobalService } from "src/app/services/global.service";
+import { HttpFetcherService } from "src/app/services/peca/http-fetcher.service";
+import {
+  profileDataToSponsorFormMapper,
+  profileDataToCordinatorFormMapper,
+  profileDataToSchoolFormMapper
+} from "../mappers/profile-mappers";
 
 @Component({
-    selector: 'peca-profile',
-    templateUrl: '../peca-page.component.html',
+  selector: "peca-profile",
+  templateUrl: "../peca-page.component.html"
 })
-export class ProfilePageComponent extends PecaPageComponent implements AfterViewInit {
-    @ViewChild('blocksContainer', { read: ViewContainerRef, static: false }) container: ViewContainerRef;
+export class ProfilePageComponent extends PecaPageComponent
+  implements AfterViewInit {
+  @ViewChild("blocksContainer", { read: ViewContainerRef, static: false })
+  container: ViewContainerRef;
+  @Select(PecaState.getUser) userData$: Observable<any>;
+  userDataSubscription: Subscription;
+  userFormData: any;
+  idUser = "";
+  userType = "";
 
-    constructor(factoryResolver: ComponentFactoryResolver) {
-        super(factoryResolver);
-        this.instantiateComponent(config);
+  constructor(
+    factoryResolver: ComponentFactoryResolver,
+    globals: GlobalService,
+    private httpFetcherService: HttpFetcherService
+  ) {
+    super(factoryResolver);
+    this.getUser();
+    globals.blockIntancesEmitter.subscribe(blocks => {
+      blocks.forEach((block, name) => this.blockInstances.set(name, block));
+      console.log(this.blockInstances);
+      this.updateDataToBlocks();
+    });
+  }
+  ngOnInit() {
+    this.loadForm();
+  }
+  loadForm() {
+    if (this.userType === "4") {
+      this.instantiateComponent(configEscuela);
     }
+    if (this.userType === "3") {
+      this.instantiateComponent(configPadrino);
+    }
+    if (this.userType === "2") {
+      this.instantiateComponent(configCoordinador);
+    }
+  }
+  getUser() {
+    /*this.getGeneralInformation();
+    const info = this.httpFetcherService
+      .get(`users/${this.idUser}?userType=${this.userType}`)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.userDataSubscription = data;
+          this.setUserFormData(data, profileDataToSponsorFormMapper);
+        },
+        error => console.error(error),
+        () => {
+          info.unsubscribe();
+        }
+      );*/
+    /////////////////
+    /* this.getGeneralInformation();
+    this.userDataSubscription = this.httpFetcherService
+      .get(`users/${this.idUser}?userType=${this.userType}`)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.setUserFormData(data, profileDataToSponsorFormMapper);
+        },
+        err => {
+          console.log(err);
+        }
+      );*/
+    this.getGeneralInformation();
+    this.userDataSubscription = this.httpFetcherService
+      .get(`users/${this.idUser}?userType=${this.userType}`)
+      .subscribe(
+        data => {
+          console.log(data);
+          if (this.userType === "4") {
+            this.setUserFormData(data, profileDataToSchoolFormMapper);
+          }
+          if (this.userType === "3") {
+            this.setUserFormData(data, profileDataToSponsorFormMapper);
+          }
+          if (this.userType === "2") {
+            this.setUserFormData(data, profileDataToCordinatorFormMapper);
+          }
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
+  getGeneralInformation() {
+    const user = this.userData$.subscribe(
+      data => {
+        this.idUser = data.id;
+        this.userType = data.userType;
+      },
+      error => console.error(error),
+      () => {
+        user.unsubscribe();
+      }
+    );
+  }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.instantiateBlocks(this.container);
+    });
+  }
+  updateDataToBlocks() {
+    if (this.userType === "4") {
+      this.setBlockData("userSchoolForm", this.userFormData);
+    }
+    if (this.userType === "3") {
+      this.setBlockData("userSponsorForm", this.userFormData);
+    }
+    if (this.userType === "2") {
+      this.setBlockData("userCordinatorForm", this.userFormData);
+    }
+  }
 
-    ngAfterViewInit(): void {
-        setTimeout(() => {
-            this.instantiateBlocks(this.container);
-        });
+  setUserFormData(userData, _mapper?: Function) {
+    if (_mapper) {
+      this.userFormData = _mapper(userData);
+    } else {
+      this.userFormData = userData;
     }
+  }
+  ngOnDestroy() {
+    this.userDataSubscription.unsubscribe();
+  }
 }
