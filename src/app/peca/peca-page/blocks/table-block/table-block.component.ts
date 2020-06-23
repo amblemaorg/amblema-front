03da776fb19a8ee1795eae841c27542befc66928
Alfredo <alfredo@binauraldev.com>
2020-06-23 09:11:35 -0400

    total en componente tabla actividad especial

--- src/app/peca/peca-page/blocks/table-block/table-block.component.ts
+++ src/app/peca/peca-page/blocks/table-block/table-block.component.ts
@@ -27,6 +27,7 @@ export class TableBlockComponent implements PresentationalBlockComponent, OnInit
       hideEdit: boolean;
       hideDelete: boolean;
     };
+    total?: number;
   };
 
   // source: LocalDataSource | any;
