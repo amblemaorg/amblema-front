8354d88b7c071f1d675384d8e4d6e3f35bf9c4d3
joseaguerrero <jose.guerrero@binaural.com.ve>
2020-07-08 23:13:49 -0400

    COR: posicion y tamaño imagen en tarjeta de testimonio, posicion de premios en slider y posicion del modal

--- src/app/web/shared/testimonial-card/testimonial-card.component.ts
+++ src/app/web/shared/testimonial-card/testimonial-card.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Testimonial } from '../../../models/web/testimonial.model';
 
 @Component({
   selector: 'web-testimonial-card',
   templateUrl: './testimonial-card.component.html',
-  styleUrls: ['./testimonial-card.component.scss']
+  styleUrls: ['./testimonial-card.component.scss'],
 })
 export class TestimonialCardComponent implements OnInit {
-
   @Input() testimonial: Testimonial;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
