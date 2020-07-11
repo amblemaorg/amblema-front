6a7533dc589291f62f08c8dbe6f0052b504fd6ae
Alfredo <alfredo@binauraldev.com>
2020-07-11 11:23:30 -0400

    consumo proceso inscripcion olimpiadas matematica

--- src/app/peca/peca-page/pages/math-olympics-config.ts
+++ src/app/peca/peca-page/pages/math-olympics-config.ts
@@ -2,6 +2,7 @@ import { formResultadoEstudianteModal } from '../blocks/form-block/all-forms';
 
 const datosOlimpiadas = {
     component: 'textsbuttons',
+    name: 'olimpiadasDateText',
     settings: {
         dateOrtext: {
             text: 'Fecha de la prueba:',
@@ -11,17 +12,10 @@ const datosOlimpiadas = {
             url: '#',
             name: 'Descargar archivo',
         },
-        subtitles: [
+        subtitles: 
             {
-                text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi temporibus fugit tempore recusandae excepturi ex natus voluptatum alias architecto ratione magni accusantium vitae id tenetur doloribus cupiditate voluptatem nostrum quidem! Reprehenderit ratione perferendis iure pariatur omnis ipsam culpa autem! Quam sequi voluptatem consequatur modi dolores repellendus voluptates sint soluta!'
+                text: ''
             },
-            {
-                text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi temporibus fugit tempore recusandae excepturi ex natus voluptatum alias architecto ratione magni accusantium vitae id tenetur doloribus cupiditate voluptatem nostrum quidem! Reprehenderit ratione perferendis iure pariatur omnis ipsam culpa autem! Quam sequi voluptatem consequatur modi dolores repellendus voluptates sint soluta! Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-            },
-            {
-                text: 'Magnam, commodi temporibus fugit tempore recusandae excepturi ex natus voluptatum alias architecto ratione magni accusantium vitae id tenetur doloribus cupiditate voluptatem nostrum quidem! Reprehenderit ratione perferendis iure pariatur omnis ipsam culpa autem! Quam sequi voluptatem consequatur modi dolores repellendus voluptates sint soluta! Reprehenderit ratione perferendis iure pariatur omnis ipsam culpa autem! Quam sequi voluptatem consequatur modi dolores repellendus voluptates sint soluta!'
-            }]
-
     }
 }
 
