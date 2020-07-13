19f7be2c2e73d9b296ed854d38f54b80b6a17ea4
Alfredo <alfredo@binauraldev.com>
2020-07-13 14:38:34 -0400

    consumo tabla amblemonedas

--- src/app/peca/peca-page/pages/amblemoneda-page.component.ts
+++ src/app/peca/peca-page/pages/amblemoneda-page.component.ts
@@ -8,9 +8,13 @@ import {
 import { PecaPageComponent } from '../peca-page.component';
 import { AMBLEMONEDA_CONFIG as config } from './amblemoneda-config';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { GlobalService } from 'src/app/services/global.service';
 import { HttpFetcherService } from 'src/app/services/peca/http-fetcher.service';
+import { Select } from "@ngxs/store";
+import { PecaState } from "../../../store/states/peca/peca.state";
+import { isNullOrUndefined } from "util";
+import { amblemonedasTableMapper } from '../mappers/amblemoneda-mappers';
 
 @Component({
   selector: 'peca-amblemoneda',
@@ -20,11 +24,16 @@ export class AmblemonedaPageComponent extends PecaPageComponent implements After
   @ViewChild('blocksContainer', { read: ViewContainerRef, static: false })
   container: ViewContainerRef;
 
+  //Selectores
+  @Select(PecaState.getActivePecaContent) infoData$: Observable<any>;
+
   //subscripciones
   infoDataSubscription: Subscription;
 
   amblemonedaData: any
   text: string;
+
+  pruebaData: any;
 
   isInstanciated: boolean;
   loadedData: boolean;
@@ -34,11 +43,11 @@ export class AmblemonedaPageComponent extends PecaPageComponent implements After
 
     globals.blockIntancesEmitter.subscribe(data => {
       data.blocks.forEach((block, name) =>
-          this.blockInstances.set(name, block)
+        this.blockInstances.set(name, block)
       );
       console.log(this.blockInstances, "bloques");
       if (this.loadedData) this.updateMethods();
-  });
+    });
 
     this.instantiateComponent(config);
     // TODO: change for a code that must reload only this page component
@@ -46,21 +55,46 @@ export class AmblemonedaPageComponent extends PecaPageComponent implements After
     // router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
   ngOnInit() {
-    this.infoDataSubscription = this.httpFetcherService.get(`pecasetting/amblecoins/1`).subscribe(
+    this.infoDataSubscription = this.infoData$.subscribe(
       data => {
-        console.log(data, 'datos amblemoneda')
+        if (!isNullOrUndefined(data)) {
+          console.log(data, "data amblemonedas")
+        }
 
         this.setAmblemonedasCharla(data);
         this.setAmblemonedasCharlaData();
 
         this.setBlockData("amblemonedaCharla", this.amblemonedaData);
 
-
+        this.setAmblemonedasMapper(data.activePecaContent.lapse1.ambleCoins.sections[0], amblemonedasTableMapper);
       }, er => { console.log(er) })
   }
 
+  updateMethods(updateData: boolean = true) {
+    this.updateDataToBlocks(updateData);
+  }
+  updateDataToBlocks(updateData: boolean) {
+    if (updateData) {
+      this.setBlockData("amblemonedaTable", this.pruebaData);
+    }
+  }
+
+  setAmblemonedasMapper(dataAmblema, _mapper?: Function) {
+    if (_mapper) {
+      console.log(dataAmblema,'asdasdasdasdasdasd')
+      this.pruebaData = {
+        data: _mapper(dataAmblema),
+        isEditable: true,
+      };
+      console.log("este es el mapper de amblemoneda", this.pruebaData);
+    } else {
+      this.pruebaData = dataAmblema;
+      console.log("este NO es el mapper de lectura", this.pruebaData);
+    }
+  }
+
   setAmblemonedasCharla(data) {
-    this.text = data.teachersMeetingDescription;
+    this.text = data.activePecaContent.lapse1.ambleCoins.teachersMeetingDescription;
     console.log(this.text, "charla")
   }
 
@@ -71,8 +105,6 @@ export class AmblemonedaPageComponent extends PecaPageComponent implements After
       }
     }
   }
-
-  updateMethods(){}
 
   ngAfterViewInit(): void {
     setTimeout(() => {
