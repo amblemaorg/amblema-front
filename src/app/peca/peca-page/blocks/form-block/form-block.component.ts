7946445933b85e0efd5310a6494d5c4fa3f67b4c
Jesus <jesus@binauraldev.com>
2020-07-14 12:14:56 -0400

    editar en diagnostico

--- src/app/peca/peca-page/blocks/form-block/form-block.component.ts
+++ src/app/peca/peca-page/blocks/form-block/form-block.component.ts
@@ -611,7 +611,7 @@ export class FormBlockComponent
         'body: ', body
       );
 //console log... comentar todo el fetcher antes de probar el put
-      /* this.fetcher[method](resourcePath, body).subscribe(
+       this.fetcher[method](resourcePath, body).subscribe(
         response => {
           commonTasks();
           console.log("Form response", response);
@@ -669,7 +669,7 @@ export class FormBlockComponent
           );
           console.error(error);
         }
-      );   */
+      );   
     }
   }
 
