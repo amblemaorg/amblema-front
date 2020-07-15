d05d63361229e0cd77af3250686c2a160b02e66d
Alfredo <alfredo@binauraldev.com>
2020-07-15 16:56:39 -0400

    arreglos slider consumo

--- src/app/peca/peca-page/pages/amblemoneda-config.ts
+++ src/app/peca/peca-page/pages/amblemoneda-config.ts
@@ -24,26 +24,24 @@ const sliderAmblemoneda = {
                     image: "../../../../../assets/images/profile-leena.jpg",
                     description: "foto numero 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
                 },
-
-               
-                                {
-                                    text: "Fecha de la actividad:",
-                                    fields: { label: "Input date", placeholder: "Input date", fullwidth: false, ...controlProps.date },
-                                    image: "../../../../../assets/images/profile-oscar.jpg",
-                                    description: "foto numero 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
-                                },
-                               /*  {
-                                    text: "Fecha de la actividad:",
-                                    fields: { label: "Input date", placeholder: "Input date", fullwidth: false, ...controlProps.date },
-                                    image: "../../../../../assets/images/background-pillar-lectura.jpg",
-                                    description: "foto numero 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
-                                },
-                                {
-                                    text: "Fecha de la actividad:",
-                                    fields: { label: "Input date", placeholder: "Input date", fullwidth: false, ...controlProps.date },
-                                    image: "../../../../../assets/images/background-pillar-matematica.jpg",
-                                    description: "foto numero 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
-                                }*/
+                {
+                    text: "Fecha de la actividad:",
+                    fields: { label: "Input date", placeholder: "Input date", fullwidth: false, ...controlProps.date },
+                    image: "../../../../../assets/images/profile-oscar.jpg",
+                    description: "foto numero 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
+                },
+                {
+                    text: "Fecha de la actividad:",
+                    fields: { label: "Input date", placeholder: "Input date", fullwidth: false, ...controlProps.date },
+                    image: "../../../../../assets/images/background-pillar-lectura.jpg",
+                    description: "foto numero 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
+                },
+                /*{
+                    text: "Fecha de la actividad:",
+                    fields: { label: "Input date", placeholder: "Input date", fullwidth: false, ...controlProps.date },
+                    image: "../../../../../assets/images/background-pillar-matematica.jpg",
+                    description: "foto numero 4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus dolorum quo animi velit accusantium alias, quis esse inventore, vero veritatis aperiam corporis quos, iusto aliquid quam. Maxime doloremque repellat perferendis?   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod provident hic placeat nisi in quaerat debitis minima impedit tempore asperiores dicta, at, doloribus ducimus perferendis aliquam, incidunt quia praesentium beatae."
+                }*/
             ]
         /*{
             image: "../../../../../assets/images/background-pillar-lectura.jpg",
