1273a1613fde6c25f33076443a9743e0e32607ef
Jesus <jesus@binauraldev.com>
2020-07-16 16:31:01 -0400

    data de los docentes traidas en la preparacion de la convencion anual

--- src/app/peca/peca-page/pages/environmental-project-page.component.ts
+++ src/app/peca/peca-page/pages/environmental-project-page.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@angular/core';
 import { PecaPageComponent } from '../peca-page.component';
 import { ENVIRONMENTAL_PROJECT_CONFIG as config } from './environmental-project-config';
+import { HttpFetcherService } from 'src/app/services/peca/http-fetcher.service';
 
 @Component({
     selector: 'peca-environmental-project',
@@ -16,11 +17,23 @@ import { ENVIRONMENTAL_PROJECT_CONFIG as config } from './environmental-project-
 export class EnvironmentalProjectPageComponent extends PecaPageComponent implements AfterViewInit {
     @ViewChild('blocksContainer', { read: ViewContainerRef, static: false }) container: ViewContainerRef;
 
-    constructor(factoryResolver: ComponentFactoryResolver) {
+    constructor(factoryResolver: ComponentFactoryResolver, private httpFetcherService: HttpFetcherService) {
         super(factoryResolver);
         this.instantiateComponent(config);
     }
 
+ngOnInit(){
+this.getInfo();
+}
+
+getInfo(){
+    const info = this.httpFetcherService.get(`pecasetting/environmentalproject`).subscribe(data =>{
+        console.log("proyecto ambiental", data);
+    },
+    error => console.log(error), ()=>{
+        info.unsubscribe();
+    })
+}
     ngAfterViewInit(): void {
         setTimeout(() => {
             this.instantiateBlocks(this.container);
