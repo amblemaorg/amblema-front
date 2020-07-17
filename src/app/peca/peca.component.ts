b647a9e53b4a04bc27a124c8282c45eb9e0263bc
Fidel Aliaga <fidel.aliaga@binauraldev.com>
2020-07-17 13:36:11 -0400

    IMP: mostrando mensaje de inscribir en el lapso actual si no hay peca activo

--- src/app/peca/peca.component.ts
+++ src/app/peca/peca.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
 import {
   PECA_MENU_DEFAULT_CONFIG,
   PECA_LAPSE_OPTIONS_CONFIG
@@ -11,11 +11,6 @@ import { Observable, Subscription } from "rxjs";
 import { first } from "rxjs/internal/operators/first";
 import { take } from "rxjs/internal/operators/take";
 import cloneDeep from "lodash/cloneDeep";
-import {
-  UpdateStates,
-  UpdateMunicipalities
-} from "../store/actions/steps/residence-info.actions";
-import { GlobalService } from "../services/global.service";
 
 @Component({
   selector: "app-peca",
@@ -23,6 +18,8 @@ import { GlobalService } from "../services/global.service";
   styleUrls: ["./peca.component.scss"]
 })
 export class PecaComponent implements OnInit, OnDestroy {
+  @ViewChild('noPecaModalLauncher', {static: true}) noPecaModalLauncherBtn:any;
+
   menu = cloneDeep(PECA_MENU_DEFAULT_CONFIG);
   lapseOptionsConfig = PECA_LAPSE_OPTIONS_CONFIG;
   image_profile = "../../assets/images/profile-oscar.jpg";
@@ -52,9 +49,12 @@ export class PecaComponent implements OnInit, OnDestroy {
     this.activePecaSubscription = this.activePeca$.pipe(first()).subscribe(
       ({ activePeca }) => {
         activePecaId = activePeca.id;
-        activePecaId
-          ? this.store.dispatch([new FetchPecaContent(activePecaId)])
-          : console.error("No pudo obtenerse el PecaId activo");
+        if (activePecaId)
+          this.store.dispatch([new FetchPecaContent(activePecaId)])
+        else {
+          console.error("No pudo obtenerse el PecaId activo");
+          this.noPecaModalLauncherBtn.nativeElement.click();
+        }
       },
       error => console.error(error)
     );
