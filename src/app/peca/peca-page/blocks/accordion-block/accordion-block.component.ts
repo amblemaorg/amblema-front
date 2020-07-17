4ee1f788bac10cc9bdef5ba6079957287fabd0e4
Jesus <jesus@binauraldev.com>
2020-07-17 16:23:05 -0400

    consumo de topicos proyecto ambiental

--- src/app/peca/peca-page/blocks/accordion-block/accordion-block.component.ts
+++ src/app/peca/peca-page/blocks/accordion-block/accordion-block.component.ts
@@ -1,46 +1,63 @@
-import { Component, OnInit, AfterViewInit, ViewContainerRef, ViewChild, QueryList, ViewChildren } from '@angular/core';
-import { PageBlockComponent, StructuralBlockComponent, StructuralItem } from '../page-block.component';
-import { PageBlockFactory } from '../page-block-factory';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewContainerRef,
+  ViewChild,
+  QueryList,
+  ViewChildren,
+} from "@angular/core";
+import {
+  PageBlockComponent,
+  StructuralBlockComponent,
+  StructuralItem,
+} from "../page-block.component";
+import { PageBlockFactory } from "../page-block-factory";
 
 @Component({
-  selector: 'peca-accordion-block',
+  selector: "peca-accordion-block",
   template: `
     <nb-accordion>
-        <nb-accordion-item *ngFor="let item of settings.items; index as i">
-            <nb-accordion-item-header>
-              <nb-icon *ngIf="item.icon" [icon]="item.icon" class="title-icon"></nb-icon>              
-              {{ item.title }}
-            </nb-accordion-item-header>
-            <nb-accordion-item-body>
-                <ng-template #accordionItemBodyContainer></ng-template>
-            </nb-accordion-item-body>
-        </nb-accordion-item>
+      <nb-accordion-item *ngFor="let item of settings.items; index as i">
+        <nb-accordion-item-header>
+          <nb-icon
+            *ngIf="item.icon"
+            [icon]="item.icon"
+            class="title-icon"
+          ></nb-icon>
+          {{ item.title }}
+        </nb-accordion-item-header>
+        <nb-accordion-item-body>
+          <ng-template #accordionItemBodyContainer></ng-template>
+        </nb-accordion-item-body>
+      </nb-accordion-item>
     </nb-accordion>
   `,
-  styleUrls: ['./accordion-block.component.scss']
+  styleUrls: ["./accordion-block.component.scss"],
 })
-export class AccordionBlockComponent implements StructuralBlockComponent, OnInit, AfterViewInit {
-  @ViewChildren('accordionItemBodyContainer', { read: ViewContainerRef }) accordionItemBodyContainer: QueryList<ViewContainerRef>;
+export class AccordionBlockComponent
+  implements StructuralBlockComponent, OnInit, AfterViewInit {
+  @ViewChildren("accordionItemBodyContainer", { read: ViewContainerRef })
+  accordionItemBodyContainer: QueryList<ViewContainerRef>;
   factory: PageBlockFactory;
 
-  enviromentalArray: any;
-  type: 'structural';
+  type: "structural";
   component: string;
   settings: {
     items: StructuralItem[];
-  }
+  };
 
   constructor() {
-    this.type = 'structural';
-    this.component = 'accordion';
+    this.type = "structural";
+    this.component = "accordion";
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.instantiateChildBlocks();
-    })
+    });
   }
 
   public setSettings(settings: any) {
@@ -49,38 +66,37 @@ export class AccordionBlockComponent implements StructuralBlockComponent, OnInit
   }
 
   setData(data: any) {
-    //console.log("sadsdsds", this.prueba)
-    if (data["topics"]) {
-      this.enviromentalArray= data.topics;
-/*       this.prueba = data.checkList[0].description;
-      this.flag = true;
-      for (let i = 0; i < this.prueba.length; i++) {
-        this.settings.infoContainer[0].datosNivel[0].checkList[i].description =
-          data.checkList[0].description[i].name; */
-        /* console.log("no se", this.settings.infoContainer[0].checkList[i])
-      console.log("checkkk",data.checkList[0]); */
-
-      for (let i = 0; i < this.enviromentalArray.length; i++) {
-        this.settings.items[i].title= data.topics[i].name;
+    if (data["topics1"]) {
+      for (let i = 0; i < data.topics1.length; i++) {
+        this.settings.items[i].title = data.topics1[i].name;
       }
-      console.log("entro", data.topics)
-      console.log("aja", this.settings.items)
-;      }
-    else {
-     // this.flag = false;
-
+      console.log("entro", data);
+      console.log("aja", this.settings.items);
+    } else if (data["topics2"]) {
+      for (let i = 0; i < data.topics2.length; i++) {
+        this.settings.items[i].title = data.topics2[i].name;
+      }
+    } else if (data["topics3"]) {
+      for (let i = 0; i < data.topics3.length; i++) {
+        this.settings.items[i].title = data.topics3[i].name;
+      }
     }
   }
   public instantiateChildBlocks() {
     this.settings.items.map((item, i) => {
       const container = this.accordionItemBodyContainer.toArray()[i];
-      item.childBlocks.map(block => {
+      item.childBlocks.map((block) => {
         let settings = block.settings;
-        if (block.component=="modal") settings = { settings: block.settings, factory: this.factory };
-        const pageBlockComponentFactory = this.factory.createPageBlockFactory(block.component);
-        const pageBlockComponent = container.createComponent(pageBlockComponentFactory);
+        if (block.component == "modal")
+          settings = { settings: block.settings, factory: this.factory };
+        const pageBlockComponentFactory = this.factory.createPageBlockFactory(
+          block.component
+        );
+        const pageBlockComponent = container.createComponent(
+          pageBlockComponentFactory
+        );
         pageBlockComponent.instance.setSettings(settings);
-      })
-    })
+      });
+    });
   }
 }
