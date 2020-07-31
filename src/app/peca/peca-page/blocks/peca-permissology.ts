6aba8711a13343075180bbccca6eb12491951e93
Fidel Aliaga <fidel.aliaga@binauraldev.com>
2020-07-30 21:18:51 -0400

    archivo con permisologia separado por entidades

---
+++ src/app/peca/peca-page/blocks/peca-permissology.ts
@@ -0,0 +1,177 @@
+export const genericActivityPermissions = {
+    name: "ActivityPeca",
+    actions: [
+        "activity_peca_view",
+        "activity_peca_edit",
+    ]
+}
+
+export const sliderActivitiesPermissions = {
+    name: "ActivitiesSlider",
+    actions: [
+        "activities_slider_view",
+        "activities_slider_create",
+        "activities_slider_edit",
+        "activities_slider_delete"
+    ]
+}
+
+export const amblecoinsPermissions = {
+    name: "AmblecoinsPeca",
+    actions: [
+        "amblecoins_peca_view",
+        "amblecoins_peca_edit"
+    ]
+}
+
+export const annualConventionPermissions = {
+    name: "AnnualConventionPeca",
+    actions: [
+        "annual_convention_peca_view",
+        "annual_convention_peca_edit"
+    ]
+}
+
+export const annualPreparationPermissions = {
+    name: "AnnualPreparationPeca",
+    actions: [
+        "annual_preparation_peca_view",
+        "annual_preparation_peca_edit"
+    ]
+}
+
+export const initialWorkshopPermissions = {
+    name: "InitialWorkshopPeca",
+    actions: [
+        "initial_workshop_peca_view",
+        "initial_workshop_peca_edit"
+    ]
+}
+
+export const lapsePlanningPermissions = {
+    name: "LapsePlanningPeca",
+    actions: [
+        "lapse_planning_peca_view",
+        "lapse_planning_peca_edit"
+    ]
+}
+
+export const olympicsPermissions = {
+    name: "OlympicsPeca",
+    actions: [
+        "olympics_peca_view",
+        "olympics_peca_edit"
+    ]
+}
+
+export const schoolPermissions = {
+    name: "SchoolPeca",
+    actions: [
+        "school_peca_view",
+        "school_peca_edit"
+    ]
+}
+
+export const teacherPermissions = {
+    name: "Teacher",
+    actions: [
+        "teacher_view",
+        "teacher_create",
+        "teacher_edit",
+        "teacher_delete"
+    ]
+}
+
+export const teacherTestimonialPermissions = {
+    name: "TeacherTestimonial",
+    actions: [
+        "teacher_testimonial_view",
+        "teacher_testimonial_create",
+        "teacher_testimonial_edit",
+        "teacher_testimonial_delete"
+    ]
+}
+
+export const sectionPermissions = {
+    name: "Section",
+    actions: [
+        "section_view",
+        "section_create",
+        "section_edit",
+        "section_delete"
+    ]
+}
+
+export const studentPermissions = {
+    name: "Student",
+    actions: [
+        "student_view",
+        "student_create",
+        "student_edit",
+        "student_delete"
+    ]
+}
+
+export const yearbookPermissions = {
+    name: "Yearbook",
+    actions: [
+        "yearbook_view",
+        "yearbook_edit"
+    ]
+}
+
+export const specialActivityPermissions = {
+    name: "SpecialActivity",
+    actions: [
+        "special_activity_view",
+        "special_activity_edit"
+    ]
+}
+
+export const adminUserPermissions = {
+    name: "AdminUser",
+    actions: [
+        "admin_view",
+        "admin_edit",
+    ]
+}
+
+export const coordinatorUserPermissions = {
+    name: "CoordinatorUser",
+    actions: [
+        "coordinator_user_view",
+        "coordinator_user_edit",
+    ]
+}
+
+export const schoolUserPermissions = {
+    name: "SchoolUser",
+    actions: [
+        "school_user_view",
+        "school_user_edit",
+    ]
+}
+
+export const sponsorUserPermissions = {
+    name: "SponsorUser",
+    actions: [
+        "sponsor_user_view",
+        "sponsor_user_edit",
+    ]
+}
+
+export const environmentalProjectPermissions = {
+    name: "EnvironmentalProject",
+    actions: [
+        "environmental_project_view",
+        "environmental_project_edit"
+    ]
+}
+
+export const monitoringActivityPermissions = {
+    name: "MonitoringActivity",
+    actions: [
+        "monitoring_activity_view",
+        "monitoring_activity_edit"
+    ]
+}
