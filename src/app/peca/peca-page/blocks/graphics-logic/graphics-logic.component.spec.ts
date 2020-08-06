d80dd0c19c080b79c7adc9190c4d61df3ee68fbd
Alfredo <alfredo@binauraldev.com>
2020-08-06 09:07:21 -0400

    test componente graficas

--- src/app/peca/peca-page/blocks/graphics-logic/graphics-logic.component.spec.ts
+++ src/app/peca/peca-page/blocks/graphics-logic/graphics-logic.component.spec.ts
@@ -1,21 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphicsLogicComponent } from './graphics-logic.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PageBlockFactory } from '../page-block-factory';
+import { ComponentFactoryResolver, ComponentFactory } from '@angular/core';
+import { PageBlockComponent } from '../page-block.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 describe('GraphicsLogicComponent', () => {
   let component: GraphicsLogicComponent;
   let fixture: ComponentFixture<GraphicsLogicComponent>;
 
-  beforeEach(async(() => {
+  let factoryResolver: ComponentFactoryResolver;
+  let pageBlockFactory: ComponentFactory<PageBlockComponent>;
+
+  let factory: PageBlockFactory = new PageBlockFactory(factoryResolver);
+  let graphicsSettings = {
+    component: 'graphics-logic',
+    settings: {
+        items: [],
+    }
+  }
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ GraphicsLogicComponent ]
+      declarations: [ 
+        GraphicsLogicComponent 
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+      ]
     })
-    .compileComponents();
-  }));
+
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [GraphicsLogicComponent]
+      }
+    });
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GraphicsLogicComponent);
     component = fixture.componentInstance;
+    factoryResolver = fixture.debugElement.injector.get(ComponentFactoryResolver);
+    component.setSettings(graphicsSettings);
     fixture.detectChanges();
   });
 
