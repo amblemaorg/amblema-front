6a1764ff6cba8a44486f3728136cbdfbcaad775a
Alfredo <alfredo@binauraldev.com>
2020-08-06 09:07:21 -0400

    test componente graficas

--- src/app/peca/peca-page/blocks/graphics-mathe-block/graphics-mathe-block.component.spec.ts
+++ src/app/peca/peca-page/blocks/graphics-mathe-block/graphics-mathe-block.component.spec.ts
@@ -1,21 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GraphicsMatheBlockComponent } from './graphics-mathe-block.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PageBlockFactory } from '../page-block-factory';
+import { ComponentFactoryResolver, ComponentFactory } from '@angular/core';
+import { PageBlockComponent } from '../page-block.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 describe('GraphicsMatheBlockComponent', () => {
   let component: GraphicsMatheBlockComponent;
   let fixture: ComponentFixture<GraphicsMatheBlockComponent>;
 
-  beforeEach(async(() => {
+  let factoryResolver: ComponentFactoryResolver;
+  let pageBlockFactory: ComponentFactory<PageBlockComponent>;
+  //factoryResolver.resolveComponentFactory.and.returnValue(pageBlockFactory)
+
+
+  let factory: PageBlockFactory = new PageBlockFactory(factoryResolver);
+  let graphicsSettings = {
+    component: 'graphics-mathe',
+    settings: {
+        items: [],
+    }
+  }
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ GraphicsMatheBlockComponent ]
+      declarations: [ 
+        GraphicsMatheBlockComponent 
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+      ]
     })
-    .compileComponents();
-  }));
+
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [GraphicsMatheBlockComponent]
+      }
+    });
+
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GraphicsMatheBlockComponent);
     component = fixture.componentInstance;
+    factoryResolver = fixture.debugElement.injector.get(ComponentFactoryResolver);
+    component.setSettings(graphicsSettings);
     fixture.detectChanges();
   });
 
