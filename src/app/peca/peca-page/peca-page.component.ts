38f32888b7084703c09a6fdb5936147b177df9fa
Fidel Aliaga <fidel.aliaga@binauraldev.com>
2020-08-07 14:45:38 -0400

    capturando error de promesa en pdf

--- src/app/peca/peca-page/peca-page.component.ts
+++ src/app/peca/peca-page/peca-page.component.ts
@@ -165,112 +165,134 @@ export class PecaPageComponent {
   }
   
   //#region Generate PDF
-  public async generatePDF() {
+  public generatePDF() {
     this.creatingPdf = true;
 
-    this.instantiatePdfFonts(); // instantianting pdf font family
-    const pdf = new PdfMakeWrapper(); // pdf document definition instance
-    const pdfPageSizes = {
-      width: 792,
-      height: 612
+    const generateThisPdf = async () => {
+      clearInterval(interval);
+      this.instantiatePdfFonts(); // instantianting pdf font family
+      const pdf = new PdfMakeWrapper(); // pdf document definition instance
+      const pdfPageSizes = {
+        width: 792,
+        height: 612
+      };
+
+      // pdf metadata
+      pdf.info({
+          title: 'AmbLeMario',
+          author: 'AmbLeMa',
+          subject: 'Anuario',
+      });
+
+      // pdf page configurations
+      pdf.pageSize('LETTER');
+      pdf.pageOrientation('landscape');
+      
+      /**
+       * pdf background, when cover page the background is blue
+       */
+      pdf.background((currentPage, pageSize) => {
+        pdfPageSizes.width = pageSize.width;
+        pdfPageSizes.height = pageSize.height;
+
+        if (currentPage === 1) 
+          return new Canvas([
+            new Rect(0, [pdfPageSizes.width, pdfPageSizes.height]).color('#00809A').end
+          ]).end
+
+        return null
+      });
+
+      // local images to get transformed into base64 format
+      const open_book = await this.getBase64FromImg("../../../assets/images/pdf/open-book.png");
+      const amble_logo = await this.getBase64FromImg("../../../assets/images/pdf/amblelogo.png");
+      // const sponsor_logo = await this.getBase64FromImg("../../../assets/images/pdf/sponsorlogo.png");
+
+      // pdf images library
+      if (this.pdfData["sponsorLogo"]) pdf.images({
+        openBook: open_book ? await new Img(open_book).build() : null,
+        ambleLogo: amble_logo ? await new Img(amble_logo).build() : null,
+        sponsorLogo: this.pdfData["sponsorLogo"] ? await new Img(this.pdfData.sponsorLogo).build() : null,
+      });
+      else pdf.images({
+        openBook: open_book ? await new Img(open_book).build() : null,
+        ambleLogo: amble_logo ? await new Img(amble_logo).build() : null,
+      });
+
+      // loading images for pdf footer use
+      // const cover_footer = open_book ? await new Img('openBook', true).width(990).margin([-105,-258,0,0]).build() : null;
+      const cover_footer = open_book ? await new Img('openBook', true).width(pdfPageSizes.width+180).margin([-80,-255,0,0]).build() : null;
+
+      /**
+       * pdf footer, when cover page opened book appears
+       */
+      pdf.footer((currentPage, pageSize) => {
+        if (currentPage === 1) 
+          return cover_footer;
+
+        return null
+      });
+
+      //* PDF CONTENT blocks --------------------------------------------------
+      pdf.add(new Stack([ 
+        this.pdfData["schoolYear"] ? new Txt(this.pdfData.schoolYear).fontSize(21).margin([0,0,0,5]).end : null,
+        this.pdfData["sponsorName"] ? this.pdfData.sponsorName.toUpperCase() : null 
+      ]).alignment('center').relativePosition(0,-20).style('coverHeader').bold().end);
+
+      if (amble_logo) pdf.add(new Canvas([new Polyline([{ x: 0, y: 0 },{ x: 0, y: 90 },{ x: 41, y: 115 },{ x: 45, y: 116 },{ x: 49, y: 115 },{ x: 90, y: 90 },{ x: 90, y: 0 }])
+        .closePath().color('#fff').end]).absolutePosition(45,0).end);
+      if (this.pdfData["sponsorLogo"]) pdf.add(new Canvas([new Polyline([{ x: 0, y: 0 },{ x: 0, y: 90 },{ x: 41, y: 115 },{ x: 45, y: 116 },{ x: 49, y: 115 },{ x: 90, y: 90 },{ x: 90, y: 0 }])
+        .closePath().color('#fff').end]).absolutePosition(pdfPageSizes.width-135,0).end);
+
+      if (amble_logo) pdf.add(await new Img('ambleLogo', true).fit([72,72]).absolutePosition(54,15).build());
+      if (this.pdfData["sponsorLogo"]) pdf.add(await new Img('sponsorLogo', true).fit([72,72]).absolutePosition(pdfPageSizes.width-126,15).build());
+
+      pdf.add(new Stack([ 
+        new Txt('AmbLeMario').bold().fontSize(62).end, 
+        this.pdfData["schoolName"] ? new Txt(this.pdfData.schoolName).bold().fontSize(16).end : null, 
+        this.pdfData["schoolName"] ? new Canvas([
+          new Rect(0, [ (185 * this.pdfData.schoolName.length) / 21 , 1]).color('#81b03e').end
+        ]).end : null,
+        this.pdfData["schoolCity"] ? new Txt(this.pdfData.schoolCity).bold().margin([0,4]).end : null, 
+      ])
+      .alignment('center').margin([0,135,0,0]).color('#fff').end);
+      
+      pdf.add(new Txt('Indice').pageBreak('before').end);
+      //* endOf PDF CONTENT blocks --------------------------------------------
+
+      pdf.styles({
+        coverHeader: {
+          fontSize: 16,
+          color: '#fff',
+        },
+        headerSchoolName: {
+          fontSize: 16,
+          decoration: 'underline',
+          decorationColor: '#81b03e',
+        }
+      });
+
+      // PDF saving methods --
+      pdf.create().open();
+      // pdf.create().download('AmbLeMario');
+
+      this.creatingPdf = false;
     };
 
-    // pdf metadata
-    pdf.info({
-        title: 'AmbLeMario',
-        author: 'AmbLeMa',
-        subject: 'Anuario',
-    });
+    let interval = null;
 
-    // pdf page configurations
-    pdf.pageSize('LETTER');
-    pdf.pageOrientation('landscape');
+    try {
+      generateThisPdf();
+    } catch (error) {
+      interval = setInterval(() => {
+        try {
+          generateThisPdf();
+        } catch (error) {
+          // TODO --
+        }
+      }, 2000);
+    }
     
-    /**
-     * pdf background, when cover page the background is blue
-     */
-    pdf.background((currentPage, pageSize) => {
-      pdfPageSizes.width = pageSize.width;
-      pdfPageSizes.height = pageSize.height;
-
-      if (currentPage === 1) 
-        return new Canvas([
-          new Rect(0, [pdfPageSizes.width, pdfPageSizes.height]).color('#00809A').end
-        ]).end
-
-      return null
-    });
-
-    // local images to get transformed into base64 format
-    const open_book = await this.getBase64FromImg("../../../assets/images/pdf/open-book.png");
-    const amble_logo = await this.getBase64FromImg("../../../assets/images/pdf/amblelogo.png");
-    // const sponsor_logo = await this.getBase64FromImg("../../../assets/images/pdf/sponsorlogo.png");
-    
-    // pdf images library
-    pdf.images({
-      openBook: open_book ? await new Img(open_book).build() : null,
-      ambleLogo: amble_logo ? await new Img(amble_logo).build() : null,
-      sponsorLogo: this.pdfData["sponsorLogo"] ? await new Img(this.pdfData.sponsorLogo).build() : null,
-    });
-
-    // loading images for pdf footer use
-    // const cover_footer = open_book ? await new Img('openBook', true).width(990).margin([-105,-258,0,0]).build() : null;
-    const cover_footer = open_book ? await new Img('openBook', true).width(pdfPageSizes.width+180).margin([-80,-255,0,0]).build() : null;
-
-    /**
-     * pdf footer, when cover page opened book appears
-     */
-    pdf.footer((currentPage, pageSize) => {
-      if (currentPage === 1) 
-        return cover_footer;
-
-      return null
-    });
-
-    //* PDF CONTENT blocks --------------------------------------------------
-    pdf.add(new Stack([ 
-      this.pdfData["schoolYear"] ? new Txt(this.pdfData.schoolYear).fontSize(21).margin([0,0,0,5]).end : null,
-      this.pdfData["sponsorName"] ? this.pdfData.sponsorName.toUpperCase() : null 
-    ]).alignment('center').relativePosition(0,-20).style('coverHeader').bold().end);
-
-    pdf.add(new Canvas([new Polyline([{ x: 0, y: 0 },{ x: 0, y: 90 },{ x: 41, y: 115 },{ x: 45, y: 116 },{ x: 49, y: 115 },{ x: 90, y: 90 },{ x: 90, y: 0 }])
-      .closePath().color('#fff').end]).absolutePosition(45,0).end);
-    pdf.add(new Canvas([new Polyline([{ x: 0, y: 0 },{ x: 0, y: 90 },{ x: 41, y: 115 },{ x: 45, y: 116 },{ x: 49, y: 115 },{ x: 90, y: 90 },{ x: 90, y: 0 }])
-      .closePath().color('#fff').end]).absolutePosition(pdfPageSizes.width-135,0).end);
-
-    pdf.add(await new Img('ambleLogo', true).fit([72,72]).absolutePosition(54,15).build());
-    pdf.add(await new Img('sponsorLogo', true).fit([72,72]).absolutePosition(pdfPageSizes.width-126,15).build());
-
-    pdf.add(new Stack([ 
-      new Txt('AmbLeMario').bold().fontSize(62).end, 
-      this.pdfData["schoolName"] ? new Txt(this.pdfData.schoolName).bold().fontSize(16).end : null, 
-      this.pdfData["schoolName"] ? new Canvas([
-        new Rect(0, [ (185 * this.pdfData.schoolName.length) / 21 , 1]).color('#81b03e').end
-      ]).end : null,
-      this.pdfData["schoolCity"] ? new Txt(this.pdfData.schoolCity).bold().margin([0,4]).end : null, 
-    ])
-    .alignment('center').margin([0,135,0,0]).color('#fff').end);
-    
-    pdf.add(new Txt('Indice').pageBreak('before').end);
-    //* endOf PDF CONTENT blocks --------------------------------------------
-
-    pdf.styles({
-      coverHeader: {
-        fontSize: 16,
-        color: '#fff',
-      },
-      headerSchoolName: {
-        fontSize: 16,
-        decoration: 'underline',
-        decorationColor: '#81b03e',
-      }
-    });
-
-    // PDF saving methods --
-    pdf.create().open();
-    // pdf.create().download('AmbLeMario');
-
-    this.creatingPdf = false;
   }
   //#endregion
 
