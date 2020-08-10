15c16fad4cfa9ef15a429f077fad6d1a4aaefc0c
Fidel Aliaga <fidel.aliaga@binauraldev.com>
2020-08-10 02:34:59 -0400

    MOD: Agregando paginas de escuela y secciones
    tambien se manejo la carga de imagenes dimanicas dentro de recorrido de un loop

--- src/app/peca/peca-page/mappers/amblemario-mapper.ts
+++ src/app/peca/peca-page/mappers/amblemario-mapper.ts
@@ -14,6 +14,41 @@ export function amblemarioMapper(pecaData) {
         city: pecaData.school && pecaData.school.addressCity ? pecaData.school.addressCity : null,
     };
 
+    const schoolSections = pecaData.school && pecaData.school.sections && pecaData.school.sections.length > 0 
+        ? pecaData.school.sections.map((section) => {
+                const {
+                    grade,
+                    image,
+                    name
+                } = section;
+
+                const sectionName = grade === "0" 
+                    ? `Preescolar, sección: ${name.toUpperCase()}` 
+                    : grade === "1" || grade === "3"
+                        ? `${grade}er Grado, sección: ${name.toUpperCase()}`
+                        : grade === "2" 
+                            ? `${grade}do Grado, sección: ${name.toUpperCase()}` 
+                            : `${grade}to Grado, sección: ${name.toUpperCase()}`;
+
+                const sectionStudents = section.students && section.students.length > 0 
+                    ? section.students.map((student) => {
+                        const {
+                            firstName,
+                            lastName
+                        } = student;
+
+                        return `${firstName} ${lastName}`
+                    }) 
+                    : null;
+
+                return {
+                    sectionName,
+                    sectionImg: image ? image : null,
+                    sectionStudents
+                }
+            }) 
+        : null;
+
     return {
         schoolYear: schoolYearName ? schoolYearName.split("lar").pop().trim() : null,
         sponsorName: sponsor && sponsor.name ? sponsor.name : null,
@@ -23,9 +58,12 @@ export function amblemarioMapper(pecaData) {
         coordinatorImg: coordinator && coordinator.image ? coordinator.image : null,
         coordinatorText: coordinator && coordinator.content ? coordinator.content : null,
         schoolName: school && school.name ? school.name : null,
+        schoolImg: school && school.image ? school.image : null,
+        schoolText: school && school.content ? school.content : null,
         schoolCity: schoolData.city,
         historicalReviewName: historicalReview && historicalReview.name ? historicalReview.name : "Reseña Histórica",
         historicalReviewText: historicalReview && historicalReview.content ? historicalReview.content : null,
         historicalReviewImg: historicalReview && historicalReview.image ? historicalReview.image : null,
+        schoolSections,
     };    
 }
