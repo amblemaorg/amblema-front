058ac7c44d7df6284cab3a923da68edb67a4f7c6
franklinp <franklin@binauraldev.com>
2020-08-12 10:50:32 -0400

    WIP: State manager del yearbook creado

--- src/app/peca/peca-page/pages/yearbook-page.component.ts
+++ src/app/peca/peca-page/pages/yearbook-page.component.ts
@@ -9,7 +9,7 @@ import {
   OnInit,
   OnDestroy,
 } from "@angular/core";
-import { Select } from "@ngxs/store";
+import { Select, Store } from "@ngxs/store";
 import { DOCUMENT } from "@angular/common";
 import { PecaPageComponent } from "../peca-page.component";
 import { MapperYearBookWeb } from "./yearbook-config";
@@ -17,7 +17,8 @@ import { PecaState } from "../../../store/states/peca/peca.state";
 import { Observable, Subscription } from "rxjs";
 import { GlobalService } from "../../../services/global.service";
 import { amblemarioMapper } from "../mappers/amblemario-mapper";
-import { PdfYearbookService } from 'src/app/services/peca/pdf-yearbook.service';
+import { PdfYearbookService } from "src/app/services/peca/pdf-yearbook.service";
+import { SetYearBook } from "src/app/store/yearbook/yearbook.action";
 
 @Component({
   selector: "peca-yearbook",
@@ -41,11 +42,12 @@ export class YearbookPageComponent extends PecaPageComponent
   title: string;
 
   constructor(
+    private store: Store,
     factoryResolver: ComponentFactoryResolver,
     pdfYearbookService: PdfYearbookService,
     globals: GlobalService
-) {
-    super(factoryResolver,null,null,pdfYearbookService);
+  ) {
+    super(factoryResolver, null, null, pdfYearbookService);
 
     globals.blockIntancesEmitter.subscribe((data) => {
       data.blocks.forEach((block, name) =>
@@ -72,7 +74,12 @@ export class YearbookPageComponent extends PecaPageComponent
             /**
              * @author Franklin Perdomo
              */
-            
+
+            // -- Save Year Book State
+            this.store.dispatch(
+              new SetYearBook(data.activePecaContent.yearbook)
+            );
+
             this.instantiateComponent(
               MapperYearBookWeb(data.activePecaContent.yearbook)
             );
---
+++ src/app/store/yearbook/yearbook.model.ts
@@ -0,0 +1,40 @@
+export interface YearBook {
+  historicalReview?: HistoricalReview;
+  sponsor?: UserContentReview;
+  school?: UserContentReview;
+  coordinator?: UserContentReview;
+  isInApproval?: boolean;
+  approvalHistory?: any[];
+  sections?: Section[];
+  lapse1?: Lapse;
+  lapse2?: Lapse;
+  lapse3?: Lapse;
+}
+
+interface Lapse {
+  activities?: Activity[];
+  diagnosticAnalysis?: string;
+}
+
+interface Activity {
+  id?: string;
+  name?: string;
+  images?: string[];
+  description?: string;
+}
+
+interface Section {
+  id?: string;
+  image?: string;
+}
+
+interface UserContentReview {
+  name?: string;
+  image?: string;
+  content?: string;
+}
+
+interface HistoricalReview {
+  image?: string;
+  content?: string;
+}
