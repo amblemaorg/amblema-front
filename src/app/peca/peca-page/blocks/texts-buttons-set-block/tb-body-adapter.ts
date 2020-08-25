97d98e3e164ff6540b77eb5115639c8e8957e32f
José Guerrero <jose.guerrero@binaural.com.ve>
2020-08-25 13:46:22 -0400

    IMP: solicitud de testimonio de los docentes

--- src/app/peca/peca-page/blocks/texts-buttons-set-block/tb-body-adapter.ts
+++ src/app/peca/peca-page/blocks/texts-buttons-set-block/tb-body-adapter.ts
@@ -1,46 +1,79 @@
-export function textsAndButtonsAdaptBody(buttonCode: string, tAndFData: {table: any[]; form: any;}) {
-    const body_adapted = {};
+export function textsAndButtonsAdaptBody(
+  buttonCode: string,
+  tAndFData: { table: any[]; form: any }
+) {
+  let body_adapted = {};
 
-    switch (buttonCode) {        
-        case 'schoolDataConfigRegistroEscuela':
-            console.log(tAndFData);
-            Object.keys(tAndFData.form).map( key => {
-                if (tAndFData.form[key] && key != "imageGroup")
-                    body_adapted[
-                        key == "nameEscuela" ? "name" 
-                        : key == "codigoEscuela" ? "code" 
-                        : key == "callesEscuela" ? "address"
-                        : key == "ciudadEscuela" ? "addressCity"
-                        : key == "nameDirector" ? "principalFirstName"
-                        : key == "lastnameDirector" ? "principalLastName"
-                        : key == "phoneDirector" ? "principalPhone"
-                        : key == "emailDirector" ? "principalEmail"
-                        : key == "nameSubDirector" ? "subPrincipalFirstName"
-                        : key == "lastnameSubDirector" ? "subPrincipalLastName"
-                        : key == "phoneSubDirector" ? "subPrincipalPhone"
-                        : key == "emailSubDirector" ? "subPrincipalEmail"
-                        : key == "number1" ? "nTeachers"
-                        : key == "number2" ? "nAdministrativeStaff"
-                        : key == "number3" ? "nLaborStaff"
-                        : key == "number4" ? "nStudents"
-                        : key == "number5" ? "nGrades"
-                        : key
-                    ] = tAndFData.form[key];
-            });
-            body_adapted["slider"] = tAndFData.table.map( slide => {
-                const slide_ = {};
-                const hasAutoId = slide.id.substring(0, 5) === "auto-";
-                if (!hasAutoId) slide_['id'] = slide.id;
-                slide_['description'] = slide.description;
-                slide_['image'] = slide.source;
+  switch (buttonCode) {
+    case "schoolDataConfigRegistroEscuela":
+      console.log(tAndFData);
+      Object.keys(tAndFData.form).map((key) => {
+        if (tAndFData.form[key] && key != "imageGroup")
+          body_adapted[
+            key == "nameEscuela"
+              ? "name"
+              : key == "codigoEscuela"
+              ? "code"
+              : key == "callesEscuela"
+              ? "address"
+              : key == "ciudadEscuela"
+              ? "addressCity"
+              : key == "nameDirector"
+              ? "principalFirstName"
+              : key == "lastnameDirector"
+              ? "principalLastName"
+              : key == "phoneDirector"
+              ? "principalPhone"
+              : key == "emailDirector"
+              ? "principalEmail"
+              : key == "nameSubDirector"
+              ? "subPrincipalFirstName"
+              : key == "lastnameSubDirector"
+              ? "subPrincipalLastName"
+              : key == "phoneSubDirector"
+              ? "subPrincipalPhone"
+              : key == "emailSubDirector"
+              ? "subPrincipalEmail"
+              : key == "number1"
+              ? "nTeachers"
+              : key == "number2"
+              ? "nAdministrativeStaff"
+              : key == "number3"
+              ? "nLaborStaff"
+              : key == "number4"
+              ? "nStudents"
+              : key == "number5"
+              ? "nGrades"
+              : key
+          ] = tAndFData.form[key];
+      });
+      body_adapted["slider"] = tAndFData.table.map((slide) => {
+        const slide_ = {};
+        const hasAutoId = slide.id.substring(0, 5) === "auto-";
+        if (!hasAutoId) slide_["id"] = slide.id;
+        slide_["description"] = slide.description;
+        slide_["image"] = slide.source;
 
-                return slide_
-            });
-            break;
-    
-        default:
-            break;
-    }
-    console.log(body_adapted);
-    return body_adapted
+        return slide_;
+      });
+      break;
+    case "dataTestimonioDocenteTabla":
+      const testimonials = tAndFData.table.map((teacher) => {
+        const { id, description, cargo, source } = teacher;
+        return {
+          teacherId: id,
+          image: source,
+          position: cargo,
+          description,
+        };
+      });
+      body_adapted = {
+        testimonials,
+      };
+      break;
+    default:
+      break;
+  }
+  console.log(body_adapted);
+  return body_adapted;
 }
