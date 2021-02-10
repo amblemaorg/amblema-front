f4f0ef9907bcdca276d34ba44d21fdea6a9fe848
Yanior <yanior@binauraldev.com>
2021-02-10 15:37:19 -0400

    Recaptcha con Nonce configuracion

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -6,7 +6,7 @@ import { SharedModule } from "./shared/shared.module";
 import { AngularSvgIconModule } from "angular-svg-icon";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CarouselModule } from "ngx-owl-carousel-o";
-import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from "ng-recaptcha";
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module, RECAPTCHA_NONCE } from "ng-recaptcha";
 // Components
 import { WebComponent } from "./web.component";
 import { HeaderComponent } from "./layout/header/header.component";
@@ -31,6 +31,7 @@ import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
   ],
   providers: [
     { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.recaptchaKey },
+    { provide: RECAPTCHA_NONCE, useValue: environment.recaptchaNonce },
   ],
 })
 export class WebModule {}
