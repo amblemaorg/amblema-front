030957b517522a2969265b01a7a06a8f22bea032
yanior <yanior@binaural.com.ve>
2021-09-20 15:43:44 -0400

    fix: correccion de perdida de estilos en peca al cerrar sesion

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,16 +1,14 @@
-import { Component } from '@angular/core';
-import { NbLoginComponent } from '@nebular/auth';
-import { NbIconLibraries } from '@nebular/theme';
+import { Component } from "@angular/core";
+import { NbLoginComponent } from "@nebular/auth";
+import { NbIconLibraries } from "@nebular/theme";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
+  selector: "app-login",
+  templateUrl: "./login.component.html",
 })
 export class LoginComponent extends NbLoginComponent {
-
   extraSetting = {
     signUp: false,
-    forgotPassword: false
+    forgotPassword: false,
   };
-
 }
