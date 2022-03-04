f839d4f553b67c6fd8c832fed4de1fc4ed878031
Greudys Godoy <greudys@binauraldev.com>
2022-03-04 20:00:16 +0000

    Archivos sueltos

---
+++ src/app/peca/peca-page/pages/formato-registro-estudiantes-config.ts
@@ -0,0 +1,13 @@
+export const SCHEDULE_CONFIG = {
+  header: {
+    title: "Formato para registro de estudiantes",
+  },
+  blocks: [
+    {
+      component: "format-download-block",
+      settings: {
+        items: [{}],
+      },
+    },
+  ],
+};
