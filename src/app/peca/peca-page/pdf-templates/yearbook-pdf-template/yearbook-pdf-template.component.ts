6ed78d4491796d57eecdf7e7285499379a197b5b
christopherdallar <christopherdallar1234@gmail.com>
2022-07-25 11:18:02 -0400

    WIP: Odoo#7896 Implementar Portada.
    * print when page loaded

--- src/app/peca/peca-page/pdf-templates/yearbook-pdf-template/yearbook-pdf-template.component.ts
+++ src/app/peca/peca-page/pdf-templates/yearbook-pdf-template/yearbook-pdf-template.component.ts
@@ -1,24 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: "app-yearbook-pdf-template",
-  templateUrl: "./yearbook-pdf-template.component.html",
-  styleUrls: ["./yearbook-pdf-template.component.scss"],
+  selector: 'app-yearbook-pdf-template',
+  templateUrl: './yearbook-pdf-template.component.html',
+  styleUrls: ['./yearbook-pdf-template.component.scss'],
 })
 export class YearbookPdfTemplateComponent implements OnInit {
   constructor() {}
 
-  showBtnDownload = true;
+  showBtnDownload = true
 
   ngOnInit(): void {
-    addEventListener("beforeprint", (event) => {
-      console.log("beforeprint");
-    });
+    window.print()
 
-    addEventListener("afterprint", (event) => {});
+    addEventListener('beforeprint', (event) => {
+      console.log('beforeprint')
+    })
+
+    addEventListener('afterprint', (event) => {})
   }
 
   print() {
-    window.print();
+    window.print()
   }
 }
