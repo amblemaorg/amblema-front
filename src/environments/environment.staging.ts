287a5160e16141e2c71727448a58507867c6aed8
christopherdallar <christopherdallar1234@gmail.com>
2022-08-17 10:16:26 -0400

    WIP: Odoo#8482 Ajuste: Listado de estudiantes
    * Modifyng nginx and docker-compose

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  recaptchaKey: "6LcB7OsUAAAAADp1Ja9obo2HXVmb2r1Gl0Dqlsgj",
-  recaptchaNonce: "0D3VrhyLft",
-  baseUrl: "http://157.245.131.248:10506/",
-  // baseUrl: "http://192.168.0.184:10505/", //back-franklin
-  // baseUrl: "http://190.207.178.184:10505/", //back-Greudys
-};
+  recaptchaKey: '6LcB7OsUAAAAADp1Ja9obo2HXVmb2r1Gl0Dqlsgj',
+  recaptchaNonce: '0D3VrhyLft',
+  baseUrl: 'http://167.71.187.213:10508/',
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,10 @@
 
 export const environment = {
   production: false,
-  recaptchaKey: "6LcB7OsUAAAAADp1Ja9obo2HXVmb2r1Gl0Dqlsgj", // from cuentas.soporte@binaural.com.ve
-  recaptchaNonce: "0D3VrhyLft",
-  baseUrl: "http://157.245.131.248:10506/",
-  // baseUrl: "http://157.245.131.248:10506/"
-  // baseUrl: "http://157.245.131.248:10506/",
-  // baseUrl: "http://192.168.1.103:10506/",
-  // baseUrl: "http://157.245.131.248:10506/",
-  // baseUrl: "http://192.168.0.184:10505/", //back-franklin
-  // baseUrl: "http://186.93.147.191:10505/", //back-Greudys
-};
+  recaptchaKey: '6LcB7OsUAAAAADp1Ja9obo2HXVmb2r1Gl0Dqlsgj', // from cuentas.soporte@binaural.com.ve
+  recaptchaNonce: '0D3VrhyLft',
+  baseUrl: 'http://167.71.187.213:10508/',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
