706a58a5f87426559798c61878b8c85e71a0c32c
christopherdallar <christopherdallar1234@gmail.com>
2022-08-17 09:15:51 -0400

    WIP: Odoo#8482 Ajuste: Listado de estudiantes
    * Setted new docker-compose and enviroment backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   recaptchaKey: '6LcB7OsUAAAAADp1Ja9obo2HXVmb2r1Gl0Dqlsgj', // from cuentas.soporte@binaural.com.ve
   recaptchaNonce: '0D3VrhyLft',
-  baseUrl: 'https://amblema.org/api/',
+  baseUrl: 'http://167.71.187.213:10508/',
   // baseUrl: "http://157.245.131.248:10506/"
   // baseUrl: "http://157.245.131.248:10506/",
   // baseUrl: "http://192.168.1.103:10506/",
