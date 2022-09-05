5a95020ddc0dd0371995d1ca1202656f85ef08e0
christopherdallar <christopherdallar1234@gmail.com>
2022-09-05 09:27:59 -0400

    WIP: Odoo#7898 Implementar índice y número de páginas.
    *Ccreated method to unnested items.

--- src/app/peca/peca-page/pdf-templates/yearbook-pdf-template/templatesModels/IndexTemplate.ts
+++ src/app/peca/peca-page/pdf-templates/yearbook-pdf-template/templatesModels/IndexTemplate.ts
@@ -2,11 +2,27 @@ import { Template, TemplateOptions } from './Template';
 
 export class IndexTemplate extends Template {
   listItems: any[];
+  notNestedItems: any[] = [];
 
   constructor(listItems: any[], templateOptions?: TemplateOptions) {
     super('indexTemplate', templateOptions);
 
     this.setListItems(listItems);
+    this.setNotNestedItems(listItems);
+    console.log('this.notNestedItems', this.notNestedItems);
+  }
+
+  setNotNestedItems(listItems: any[]) {
+    for (let index = 0; index < listItems.length; index++) {
+      const item = listItems[index];
+
+      if (!Array.isArray(item)) {
+        this.notNestedItems.push(item);
+        continue;
+      }
+
+      this.setNotNestedItems(item);
+    }
   }
 
   setListItems(listItems: any[]) {
