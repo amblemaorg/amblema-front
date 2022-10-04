32b6580ae5e2486fc4fd2e712557babdf22b4257
christopherdallar <christopherdallar1234@gmail.com>
2022-10-04 13:05:52 -0400

    Fix:
    * Added lapse name as subtitle when there isn't diagnostic page by lapse

--- src/app/peca/peca-page/pdf-templates/yearbook-pdf-template/templatesModels/ActivityTemplate.ts
+++ src/app/peca/peca-page/pdf-templates/yearbook-pdf-template/templatesModels/ActivityTemplate.ts
@@ -2,8 +2,9 @@ import { Template, TemplateOptions } from './Template';
 export class ActivityTemplate extends Template {
   constructor(
     public storeId: string,
-    public name: string,
+    public title: string,
     public description = '',
+    public subtitle = '',
     public images = [],
     public characterLimit = 0,
     templateOptions?: TemplateOptions,
