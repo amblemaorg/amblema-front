25b109749217fa1b4738d361ce6d31613f48b63b
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-04-22 15:42:25 -0400

    Modificacion de los graficos para que no muestren 0.01

--- src/app/web/pages/coordinators/coordinators-static-content.ts
+++ src/app/web/pages/coordinators/coordinators-static-content.ts
@@ -11,38 +11,38 @@ export const COORDINATOR_CONTENT = {
       "Tengan la disposición de aumentar su grado de satisfacción personal, familiar y/o empresarial, al ver resultados positivos en la mejora de la calidad educativa de muchos docentes y niños en escuelas con escasos recursos económicos.",
     ],
     testimonials: [
-      {
-        description:
-          "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
-        firstName: "Stephanie",
-        function: "Coordinador Principal",
-        image: "./assets/images/profile-leena.jpg",
-        lastName: "Soteldo",
-      },
-      {
-        description:
-          "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
-        firstName: "Greudys",
-        function: "Coordinador Principal",
-        image: "./assets/images/profile-oscar.jpg",
-        lastName: "Godoy",
-      },
-      {
-        description:
-          "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
-        firstName: "Franklin",
-        function: "Coordinador principal",
-        image: "./assets/images/profile-oscar.jpg",
-        lastName: "Perdomo",
-      },
-      {
-        description:
-          "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
-        firstName: "José Alberto",
-        function: "Coordinador Principal",
-        image: "./assets/images/profile-oscar.jpg",
-        lastName: "Guerrero",
-      },
+      // {
+      //   description:
+      //     "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
+      //   firstName: "Stephanie",
+      //   function: "Coordinador Principal",
+      //   image: "./assets/images/profile-leena.jpg",
+      //   lastName: "Soteldo",
+      // },
+      // {
+      //   description:
+      //     "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
+      //   firstName: "Greudys",
+      //   function: "Coordinador Principal",
+      //   image: "./assets/images/profile-oscar.jpg",
+      //   lastName: "Godoy",
+      // },
+      // {
+      //   description:
+      //     "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
+      //   firstName: "Franklin",
+      //   function: "Coordinador principal",
+      //   image: "./assets/images/profile-oscar.jpg",
+      //   lastName: "Perdomo",
+      // },
+      // {
+      //   description:
+      //     "Quiero agradecer a los maestros su compromiso, su coraje, la pasión; con la que están educando a nuestros niños.... Nunca olviden que ustedes son agentes de cambio",
+      //   firstName: "José Alberto",
+      //   function: "Coordinador Principal",
+      //   image: "./assets/images/profile-oscar.jpg",
+      //   lastName: "Guerrero",
+      // },
     ],
   },
 };
