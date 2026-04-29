7f1da032f7b00d44864f3dea6b03b5d73f3908ad
Fidel Aliaga <fidel.alejos@gmail.com>
2026-04-29 16:46:45 -0400

    build olimpiadas, foto grupal, diagnisticos

---
+++ dist/browser/assets/tests/mocks/maps.googleapis.com-maps-api.js
@@ -0,0 +1,53 @@
+(function(){
+  window.google = window.google || {
+    maps: {
+      Map: function() {
+        this.fitBounds = (bounds, padding) => {};
+        this.addListener = () => {};
+      },
+      OverlayView : function () {},
+      Geocoder: function() {},
+      Marker:  function() {
+        this.addListener = () => {};
+        this.setPosition = () => {};
+        this.setMap = () => {};
+        this.getTitle = () => {};
+      },
+      InfoWindow : function() {
+        this.setContent = () => {};
+        this.close = () => {};
+      },
+      LatLng: function(lat, lng){
+        this.lat = () => lat;
+        this.lng = () => lng;
+      },
+      Size: function() {},
+      Animation: {
+        DROP: ''
+      },
+      MapTypeId: {ROADMAP: true},
+      places: {
+        Autocomplete: function() {
+          this.addListener = () => {};
+        },
+        AutocompleteService: function() {},
+        PlacesService: function(obj){
+          return {
+            PlacesServiceStatus: {
+              OK: true
+            },
+            textSearch: function(query){
+              return [];
+            },
+            nearbySearch: function(query){
+              return [];
+            }
+          };
+        }
+      },
+      GeocoderStatus: {
+        OK: 'OK'
+      }
+    }
+  };
+})();
