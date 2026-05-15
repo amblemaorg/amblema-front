644500ac6699069764d492ee666a479ee960a549
Fidel Aliaga <fidel.alejos@gmail.com>
2026-05-15 12:40:06 -0400

    indexacion google

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,3 +1,4 @@
+import 'zone.js/dist/zone-node';
 import { enableProdMode } from '@angular/core';
 
 import { environment } from './environments/environment';
@@ -8,5 +9,23 @@ if (environment.production) {
 
 export { AppServerModule } from './app/app.server.module';
 export { ngExpressEngine } from '@nguniversal/express-engine';
-export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+export { provideModuleMap };
 
+import { renderModuleFactory as _renderModuleFactory } from '@angular/platform-server';
+
+declare const __non_webpack_require__: any;
+
+export function renderModuleFactory(moduleFactory: any, options: any) {
+  // The prerender builder calls this function. 
+  // We bypass Webpack to require the generated bundle and extract LAZY_MODULE_MAP
+  const mainExports = __non_webpack_require__('./main.js');
+  const lazyModuleMap = mainExports.LAZY_MODULE_MAP;
+  
+  options.extraProviders = options.extraProviders || [];
+  if (lazyModuleMap) {
+    options.extraProviders.push(provideModuleMap(lazyModuleMap));
+  }
+  
+  return _renderModuleFactory(moduleFactory, options);
+}
